4533ecbf92ae54341ba44f05f8c40b120a7f1cce
Andy Anderson <andy.anderson@mobiledgex.com>
2018-12-10 07:58:40 -0600

    Changes to be committed: 	new file:   modules/MexProcess/__init__.py 	new file:   modules/MexProcess/mex_process.py 	new file:   modules/MexProcess/mex_process.py.new 	new file:   modules/MexProcess/mex_process.py.orig 	modified:   testcases/dme/cloudlet/findCloudlet_azure_azureCloser.robot 	new file:   tools/create_tc_import_csv.py

---
+++ modules/MexProcess/__init__.py
@@ -0,0 +1,9 @@
+from MexProcess.mex_process import MexProcessHandler
+#from MEXProcess.mex_process import Etcd
+#from MEXProcess.mex_process import Controller
+
+print('andy')
+
+class MexProcess(MexProcessHandler):
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
+    print('MEXProcess init')
---
+++ tools/create_tc_import_csv.py
@@ -0,0 +1,121 @@
+#!/usr/local/bin/python3
+
+# copy needed proto files into 1 directory
+# I originally tried building these from the original path but ran into problems
+
+import os
+import glob
+import pathlib
+import re
+import sys
+
+outfile = 'tc_import.csv'
+
+result = 'Automated test passes'
+components = 'Automated,Controller,Flavor'
+versions = 'Nimbus'
+
+tcfile = 'test_*.py'
+if len(sys.argv) == 2:
+    tcfile = sys.argv[1]
+
+def get_file_list():
+    file_list = []
+    l = glob.glob(tcfile)
+    #print(l)
+    for f in l:
+        ff = pathlib.Path(f).resolve()
+        file_list.append(str(ff))
+    return file_list
+
+def get_tc_name(l):
+    tc_hash = {}
+    
+    tc_list = []
+    #l = os.listdir('.')
+    #print(l)
+    #l = os.walk('.')
+    #print(l)
+    #l = glob.glob('test_*.py')
+    #print(l)
+    for f in l:
+        #ff = pathlib.Path(f).resolve()
+        print(f)
+        #dl = str(ff).split('/testcases/')[1].split('/')
+        dl = f.split('/testcases/')[1].split('/')
+        #print(dl)
+        tc = ''
+        for d in dl:
+            if '.py' in d:
+                d = d.split('.py')[0] + '.tc'
+            tc += d + '.'
+        #print(tc)
+        match1 = 'def test_.+'
+        match2 = '#\s+\[Documentation].*'
+        match3 = '#\s+\.\.\..+'
+        with open(f, 'r') as tcf:
+            print('fffff',f)
+            t = tcf.read()
+            d = re.compile('({}|{}|{})'.format(match1, match2, match3)).findall(t)
+            #print(d)
+            description = ''
+            title = ''
+            current_tc = ''
+            for line in d:
+                #print('l',line)
+                if 'def test' in line:
+                    test = line[4:].split('(')[0]
+                    tc_hash[tc+test] = {'title':'', 'desc':''}
+                    description = ''
+                    current_tc = tc+test
+                    #print(current_tc)
+                elif '[Documentation]' in line:
+                    tc_hash[current_tc]['title'] = line.split('[Documentation]')[1].strip()
+                    #print('title',title)
+                else:
+                    description += line.split('...')[1].strip() + '\n'
+                    tc_hash[current_tc]['desc'] = description
+                #print(d)
+                #print('title',title,'desc',description)
+                #tc_hash[tc + test] = {'title': title, 'desc': description}
+            
+            #m = re.findall('def test_.+', t)
+            #for t2 in m:
+            #    test = t2[4:].split('(')[0]
+            #    print('t',test)
+            #    tc_list.append(tc + test)
+            #    #d = re.findall('#\s+\[Documentation].* | #\s+\.\.\..+', t)
+            #    #print(d)
+            #    #sys.exit(1)
+            #    #d = re.findall('(#\s+\[Documentation].*) | (#\s+\.\.\..+)', t, re.DEBUG)
+            #    d = re.compile('({}|{})'.format(match1, match2)).findall(t)
+            #    print(d)
+            #    description = ''
+            #    title = ''
+            #    for line in d:
+            #        #print('l',line)
+            #        if '[Documentation]' in line:
+            #            title = line.split('[Documentation]')[1].strip()
+            #            #print('title',title)
+            #        else:
+            #            description += line.split('...')[1].strip() + '\n'
+            #        #print(d)
+            #    #print('title',title,'desc',description)
+            #    tc_hash[tc + test] = {'title': title, 'desc': description}
+            #    #tc_hash['title'] = title
+            #    #tc_hash['desc'] = description
+            #    #tc_list.append(tc_hash)
+    return tc_hash
+
+file_list = get_file_list()
+tc_list = get_tc_name(file_list)
+
+
+external_id = ''
+test_data = ''
+with open(outfile,'w') as f:
+    f.write('Name,STEPS,Result,Testdata,ExternalId,Versions,Components,Description\n')
+    for t in tc_list:
+        print(t)
+        print(t, tc_list[t]['title'], tc_list[t]['desc'])
+        f.write('{},{},{},{},{},"{}","{}","{}"\n'.format(tc_list[t]['title'], t, result, test_data, external_id, versions, components, tc_list[t]['desc']))
