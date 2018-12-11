670d0795763251b8f20b992a7ceea9508bc9735a
Andy Anderson <andy.anderson@mobiledgex.com>
2018-12-11 14:41:47 -0600

    add flavor testcases  Changes to be committed: 	new file:   README.md 	new file:   __init__.py 	new file:   test_flavorAdd_100.py 	new file:   test_flavorAdd_disk0.py 	new file:   test_flavorAdd_invalidName.py 	new file:   test_flavorAdd_invalidParms.py 	new file:   test_flavorAdd_largerThanMaxValues.py 	new file:   test_flavorAdd_maxValues.py 	new file:   test_flavorAdd_minValues.py 	new file:   test_flavorAdd_multiControllers.py 	new file:   test_flavorAdd_nameOnly.py 	new file:   test_flavorAdd_noName.py 	new file:   test_flavorAdd_ram0.py 	new file:   test_flavorAdd_sameName.py 	new file:   test_flavorAdd_vcpus0.py 	new file:   test_flavorDelete_beforeClusterFlavor.py 	new file:   test_flavorDelete_nameOnly.py 	new file:   test_flavorDelete_unknown.py 	new file:   test_flavorDelete_wrongParms.py 	new file:   test_flavorShow_ramVcupsDisk.py 	new file:   test_flavorShow_single.py 	new file:   test_flavorUpdate_notSupported.py

---
+++ testcases/controller/flavor/test_flavorAdd_maxValues.py
@@ -0,0 +1,70 @@
+#!/usr/local/bin/python3
+
+#
+# create flavor with values set to largest int size 
+# verify flavor is added
+# 
+
+import unittest
+import sys
+import time
+from delayedassert import expect, expect_equal, assert_expectations
+import logging
+import os
+
+from MexController import mex_controller
+
+controller_address = os.getenv('AUTOMATION_CONTROLLER_ADDRESS', '127.0.0.1:55001')
+
+flavor_name = 'flavor' + str(int(time.time()))
+ram = 18446744073709551615  # unit64 64-bit integers (0 to 18446744073709551615) 
+disk = 18446744073709551615
+vcpus = 18446744073709551615 
+
+mex_root_cert = 'mex-ca.crt'
+mex_cert = 'localserver.crt'
+mex_key = 'localserver.key'
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+class tc(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.controller = mex_controller.Controller(controller_address = controller_address,
+                                                    root_cert = mex_root_cert,
+                                                    key = mex_key,
+                                                    client_cert = mex_cert
+                                                   )
+
+    def test_createFlavorMaxValue(self):
+        # [Documentation] Flavor - User shall be able to create a flavor with ram/disk/vcpus = largest int
+        # ... create flavor with ram/disk/vcpus = largest int size
+        # ... verify flavor is added
+
+        # print flavors before add
+        flavor_pre = self.controller.show_flavors()
+
+        # create flavor
+        error = None
+        self.flavor = mex_controller.Flavor(flavor_name=flavor_name, ram=ram, vcpus=vcpus, disk=disk)
+        self.controller.create_flavor(self.flavor.flavor)
+
+        # print flavors after add
+        flavor_post = self.controller.show_flavors()
+
+        # found flavor
+        found_flavor = self.flavor.exists(flavor_post)
+
+        expect_equal(found_flavor, True, 'find flavor')
+        expect_equal(len(flavor_post), len(flavor_pre)+1, 'num flavor')
+
+        assert_expectations()
+
+    def tearDown(self):
+        self.controller.delete_flavor(self.flavor.flavor)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(tc)
+    sys.exit(not unittest.TextTestRunner().run(suite).wasSuccessful())
+
