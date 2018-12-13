4e8f8ff37840388bc561d88888715653e9b8184d
Andy Anderson <andy.anderson@mobiledgex.com>
2018-12-13 12:28:39 -0600

    add cluster testcases  Changes to be committed: 	new file:   README.md 	new file:   __init__.py 	new file:   test_clusterAddMultiControllers.py 	new file:   test_clusterAdd_100.py 	new file:   test_clusterAdd_AutoCluster.py 	new file:   test_clusterAdd_InvalidClusterName.py 	new file:   test_clusterAdd_defaultFlavor.py 	new file:   test_clusterAdd_noDefaultFlavor.py 	new file:   test_clusterAdd_noName.py 	new file:   test_clusterDelete_beforeApp.py 	new file:   test_clusterDelete_beforeClusterInst.py 	new file:   test_clusterDelete_invalidName.py 	new file:   test_clusterDelete_nameNotFound.py 	new file:   test_clusterDelete_noName.py 	new file:   test_clusterInstAdd.py 	new file:   test_clusterInstAddMultiControllers.py 	new file:   test_clusterInstAdd_100.py 	new file:   test_clusterInstAdd_cloudletInfoNotFound.py 	new file:   test_clusterInstAdd_clusterNotFound.py 	new file:   test_clusterInstAdd_flavorNotExist.py 	new file:   test_clusterInstAdd_keyExists.py 	new file:   test_clusterInstAdd_liveness1.py 	new file:   test_clusterInstAdd_liveness2.py 	new file:   test_clusterInstAdd_missingParms.py 	new file:   test_clusterInstAdd_noDefaultFlavor_noFlavor.py 	new file:   test_clusterInstAdd_noFlavor.py 	new file:   test_clusterInstAdd_operatorNotMatchCloudlet.py 	new file:   test_clusterInstDelete.py 	new file:   test_clusterInstDelete_beforeAppInst.py 	new file:   test_clusterInstDelete_notFound.py 	new file:   test_clusterInstUpdate_notSupported.py 	new file:   test_clusterUpdate_notSupported.py

---
+++ testcases/controller/cluster/test_clusterInstAdd_operatorNotMatchCloudlet.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+
+#
+# create cluster
+# create cloudinst with cloudletname that exists in cloudlets but does not match operator_name 
+# verify Specified Cloudlet not found 
+# 
+
+import unittest
+import grpc
+import sys
+import time
+from delayedassert import expect, expect_equal, assert_expectations
+import logging
+import os
+
+from MexController import mex_controller
+
+controller_address = os.getenv('AUTOMATION_CONTROLLER_ADDRESS', '127.0.0.1:55001')
+
+stamp = str(time.time())
+operator_name = 'ATT'
+cloud_name = 'tmocloud-1'
+flavor_name = 'c1.small' + stamp
+cluster_name = 'cluster' + stamp
+
+mex_root_cert = 'mex-ca.crt'
+mex_cert = 'localserver.crt'
+mex_key = 'localserver.key'
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+class tc(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.controller = mex_controller.Controller(controller_address = controller_address,
+                                                    root_cert = mex_root_cert,
+                                                    key = mex_key,
+                                                    client_cert = mex_cert
+                                                   )
+
+        self.flavor = mex_controller.Flavor(flavor_name=flavor_name, ram=1024, vcpus=1, disk=1)
+        self.cluster_flavor = mex_controller.ClusterFlavor(cluster_flavor_name=flavor_name, node_flavor_name=flavor_name, master_flavor_name=flavor_name, number_nodes=1, max_nodes=1, number_masters=1)
+        self.cluster = mex_controller.Cluster(cluster_name=cluster_name,
+                                         default_flavor_name=flavor_name)
+
+        self.cluster_instance = mex_controller.ClusterInstance(cluster_name=cluster_name,
+                                                             cloudlet_name=cloud_name,
+                                                             operator_name=operator_name,
+                                                             flavor_name=flavor_name)
+
+        self.controller.create_flavor(self.flavor.flavor)
+        self.controller.create_cluster_flavor(self.cluster_flavor.cluster_flavor)
+
+    def test_OperatorNotMatchCloudlet(self):
+        # [Documentation] ClusterInst - User shall not be able to create a cluster instance with cloudlet that does not match operator 
+        # ... create cluster
+        # ... create cloudinst with cloudletname that exists in cloudlets but does not match operator_name
+        # ... verify Specified Cloudlet not found
+
+        # print the existing cluster instances
+        clusterinst_pre = self.controller.show_cluster_instances()
+
+        # create a new cluster for adding the instance
+        create_cluster_resp = self.controller.create_cluster(self.cluster.cluster)
+
+        # create the cluster instance with operator name that does not match cloudlet operator name 
+        try:
+            resp = self.controller.create_cluster_instance(self.cluster_instance.cluster_instance)
+        except Exception as e:
+            print('got exception', e)
+
+        # print the cluster instances after error
+        clusterinst_post = self.controller.show_cluster_instances()
+
+        expect_equal(self.controller.response.code(), grpc.StatusCode.UNKNOWN, 'status code')
+        expect_equal(self.controller.response.details(), 'Cloudlet operator_key:<name:"' + operator_name + '" > name:"' + cloud_name + '"  not ready, state is CloudletStateNotPresent', 'error details')
+        expect_equal(len(clusterinst_pre), len(clusterinst_post), 'same number of cluster')
+        assert_expectations()
+
+    def tearDown(self):
+        self.controller.delete_cluster(self.cluster.cluster)
+        self.controller.delete_cluster_flavor(self.cluster_flavor.cluster_flavor)
+        self.controller.delete_flavor(self.flavor.flavor)
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(tc)
+    sys.exit(not unittest.TextTestRunner().run(suite).wasSuccessful())
+
