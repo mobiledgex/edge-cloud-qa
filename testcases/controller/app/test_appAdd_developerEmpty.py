4a1efc8bdcd3658df9fba1b42f4b74668e872540
Andy Anderson <andy.anderson@mobiledgex.com>
2018-12-18 09:11:38 -0600

    add controller/app testcases and related mex_controller fixed  Changes to be committed: 	modified:   ../../../modules/MexController/mex_controller.py 	new file:   README.md 	new file:   __init__.py 	new file:   test_appAdd_100.py 	new file:   test_appAdd_IpAccessDedicated65535.py 	new file:   test_appAdd_IpAccessDedicatedOrSharedMulti.py 	new file:   test_appAdd_IpAccessDedicatedOrSharedPort1.py 	new file:   test_appAdd_IpAccessDedicatedOrSharedPort65535.py 	new file:   test_appAdd_IpAccessDedicatedPort1.py 	new file:   test_appAdd_IpAccessDedicatedPortMulti.py 	new file:   test_appAdd_IpAccessSharedMulti.py 	new file:   test_appAdd_IpAccessSharedPort1.py 	new file:   test_appAdd_IpAccessSharedPort65535.py 	new file:   test_appAdd_appNameEmpty.py 	new file:   test_appAdd_appNameInvalid.py 	new file:   test_appAdd_clusterNotFound.py 	new file:   test_appAdd_config.py 	new file:   test_appAdd_config_invalid.py 	new file:   test_appAdd_defaultFlavorEmpty.py 	new file:   test_appAdd_defaultFlavorNotFound.py 	new file:   test_appAdd_developerEmpty.py 	new file:   test_appAdd_developerNotFound.py 	new file:   test_appAdd_docker.py 	new file:   test_appAdd_dockerCompliant.py 	new file:   test_appAdd_emptyCluster.py 	new file:   test_appAdd_flavorNotFound.py 	new file:   test_appAdd_imagePath.py 	new file:   test_appAdd_imageTypeOnly.py 	new file:   test_appAdd_invalidImageType.py 	new file:   test_appAdd_invalidIpAccess.py 	new file:   test_appAdd_invalidPortFormat.py 	new file:   test_appAdd_keyExists.py 	new file:   test_appAdd_noIpAccess.py 	new file:   test_appAdd_noParms.py 	new file:   test_appAdd_noPorts.py 	new file:   test_appAdd_portInvalidDigits.py 	new file:   test_appAdd_portOutOfRange.py 	new file:   test_appAdd_qcow.py 	new file:   test_appAdd_unsupportedPortProtocol.py 	new file:   test_appDelete_keyNotFound.py 	new file:   test_appShow_queryParms.py

---
+++ testcases/controller/app/test_appAdd_developerEmpty.py
@@ -0,0 +1,157 @@
+#!/usr/local/bin/python3
+
+#
+# create app with developer empty and missing 
+# verify 'Invalid developer name' is received
+# 
+
+import unittest
+import grpc
+import sys
+import time
+from delayedassert import expect, expect_equal, assert_expectations
+import logging
+import os
+
+from MexController import mex_controller
+
+controller_address = os.getenv('AUTOMATION_CONTROLLER_ADDRESS', '127.0.0.1:55001')
+
+access_ports = 'tcp:1'
+
+mex_root_cert = 'mex-ca.crt'
+mex_cert = 'localserver.crt'
+mex_key = 'localserver.key'
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+class tc(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.controller = mex_controller.Controller(controller_address = controller_address,
+                                                    root_cert = mex_root_cert,
+                                                    key = mex_key,
+                                                    client_cert = mex_cert
+                                                   )
+
+    def test_CreateAppDeveloperEmpty_Docker(self):
+        # [Documentation] App - User shall not be able to create app with empty developername and type Docker
+        # ... Create an app with empty developername and type Docker
+        # ... verify 'Invalid developer name' is received
+
+        # print the existing apps 
+        app_pre = self.controller.show_apps()
+
+        # create the app with no parms
+        error = None
+        app = mex_controller.App(image_type='ImageTypeDocker',
+                                 cluster_name='dummyCluster',
+                                 access_ports=access_ports,
+                                 developer_name='',
+                                 use_defaults=False)
+        try:
+            resp = self.controller.create_app(app.app)
+        except grpc.RpcError as e:
+            logger.info('got exception ' + str(e))
+            error = e
+
+        # print the cluster instances after error
+        app_post = self.controller.show_apps()
+
+        expect_equal(error.code(), grpc.StatusCode.UNKNOWN, 'status code')
+        expect_equal(error.details(), 'Invalid developer name', 'error details')
+        expect_equal(len(app_pre), len(app_post), 'same number of apps')
+        assert_expectations()
+
+    def test_CreateAppDeveloperEmpty_QCOW(self):
+        # [Documentation] App - User shall not be able to create app with empty developername and type QCOW
+        # ... Create an app with empty developername and type QCOW
+        # ... verify 'Invalid developer name' is received
+
+        # print the existing apps
+        app_pre = self.controller.show_apps()
+
+        # create the app with no parms
+        error = None
+        app = mex_controller.App(image_type='ImageTypeQCOW',
+                                 cluster_name='dummyCluster',
+                                 access_ports=access_ports,
+                                 developer_name='',
+                                 use_defaults=False)
+        try:
+            resp = self.controller.create_app(app.app)
+        except grpc.RpcError as e:
+            logger.info('got exception ' + str(e))
+            error = e
+
+        # print the cluster instances after error
+        app_post = self.controller.show_apps()
+
+        expect_equal(error.code(), grpc.StatusCode.UNKNOWN, 'status code')
+        expect_equal(error.details(), 'Invalid developer name', 'error details')
+        expect_equal(len(app_pre), len(app_post), 'same number of apps')
+        assert_expectations()
+
+    def test_CreateAppDeveloperNotExist_Docker(self):
+        # [Documentation] App - User shall not be able to create app with no developername and type Docker
+        # ... Create an app with no developername and type Docker
+        # ... verify 'Invalid developer name' is received
+
+        # print the existing apps
+        app_pre = self.controller.show_apps()
+
+        # create the app with no parms
+        error = None
+        app = mex_controller.App(image_type='ImageTypeDocker',
+                                 cluster_name='dummyCluster',
+                                 access_ports=access_ports,
+                                 use_defaults=False
+                                 )
+        try:
+            resp = self.controller.create_app(app.app)
+        except grpc.RpcError as e:
+            logger.info('got exception ' + str(e))
+            error = e
+
+        # print the cluster instances after error
+        app_post = self.controller.show_apps()
+
+        expect_equal(error.code(), grpc.StatusCode.UNKNOWN, 'status code')
+        expect_equal(error.details(), 'Invalid developer name', 'error details')
+        expect_equal(len(app_pre), len(app_post), 'same number of apps')
+        assert_expectations()
+
+    def test_CreateAppDeveloperNotExist_QCOW(self):
+        # [Documentation] App - User shall not be able to create app with no developername and type QCOW
+        # ... Create an app with no developername and type QCOW
+        # ... verify 'Invalid developer name' is received
+
+        # print the existing apps
+        app_pre = self.controller.show_apps()
+
+        # create the app with no parms
+        error = None
+        app = mex_controller.App(image_type='ImageTypeQCOW',
+                                 cluster_name='dummyCluster',
+                                 access_ports=access_ports,
+                                 use_defaults=False
+                                 )
+        try:
+            resp = self.controller.create_app(app.app)
+        except grpc.RpcError as e:
+            logger.info('got exception ' + str(e))
+            error = e
+
+        # print the cluster instances after error
+        app_post = self.controller.show_apps()
+
+        expect_equal(error.code(), grpc.StatusCode.UNKNOWN, 'status code')
+        expect_equal(error.details(), 'Invalid developer name', 'error details')
+        expect_equal(len(app_pre), len(app_post), 'same number of apps')
+        assert_expectations()
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(tc)
+    sys.exit(not unittest.TextTestRunner().run(suite).wasSuccessful())
+
---
+++ testcases/controller/app/test_appAdd_dockerCompliant.py
@@ -0,0 +1,203 @@
+#!/usr/local/bin/python3
+
+#EDGECLOUD-192 - fixed
+#create an app with app name this is not docker compliant
+#verify imagename is converted to docker compliant  - compiancy is checked in the mex_controller module
+
+import unittest
+import grpc
+import sys
+import time
+from delayedassert import expect, expect_equal, assert_expectations
+import logging
+import os
+
+from MexController import mex_controller
+
+controller_address = os.getenv('AUTOMATION_CONTROLLER_ADDRESS', '127.0.0.1:55001')
+
+stamp = str(time.time())
+image_type = 'ImageTypeDocker'
+developer_name = 'developer' + stamp
+developer_address = 'allen tx'
+developer_email = 'dev@dev.com'
+flavor_name = 'x1.small' + stamp
+cluster_name = 'cluster' + stamp
+app_name = 'app' + stamp
+app_version = '1.0'
+access_ports = 'tcp:1'
+
+mex_root_cert = 'mex-ca.crt'
+mex_cert = 'localserver.crt'
+mex_key = 'localserver.key'
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+class tc(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.controller = mex_controller.Controller(controller_address = controller_address,
+                                                    root_cert = mex_root_cert,
+                                                    key = mex_key,
+                                                    client_cert = mex_cert
+                                                   )
+
+        self.flavor = mex_controller.Flavor(flavor_name=flavor_name, ram=1024, vcpus=1, disk=1)
+        self.cluster_flavor = mex_controller.ClusterFlavor(cluster_flavor_name=flavor_name, node_flavor_name=flavor_name, master_flavor_name=flavor_name, number_nodes=1, max_nodes=1, number_masters=1)
+        self.developer = mex_controller.Developer(developer_name=developer_name,
+                                                  developer_address=developer_address,
+                                                  developer_email=developer_email)
+        self.cluster = mex_controller.Cluster(cluster_name=cluster_name,
+                                              default_flavor_name=flavor_name)
+
+        self.controller.create_flavor(self.flavor.flavor)
+        self.controller.create_cluster_flavor(self.cluster_flavor.cluster_flavor)
+        self.controller.create_developer(self.developer.developer) 
+        self.controller.create_cluster(self.cluster.cluster)
+
+    def test_CreateNameSpace(self):
+        # [Documentation] App - appname with spaces shall remove spaces in imagepath (docker compliant)
+        # ... create an app with app name with spaces that is not docker compliant
+        # ... verify imagename is converted to docker compliant with spaces removed
+
+        # print the existing apps 
+        apps_pre = self.controller.show_apps()
+
+        # create the app
+        self.app = mex_controller.App(image_type=image_type,
+                                      app_name='andy dandy',
+                                      app_version=app_version,
+                                      access_ports=access_ports,
+                                      ip_access='IpAccessShared',
+                                      cluster_name=cluster_name,
+                                      developer_name=developer_name,
+                                      default_flavor_name=flavor_name,
+                                      use_defaults=False)
+
+        resp = self.controller.create_app(self.app.app)
+
+        # print the cluster instances after error
+        apps_post = self.controller.show_apps()
+
+        # find app in list
+        self.app.image_path = 'mobiledgex_' + developer_name + '/andydandy:1.0'
+        found_app = self.app.exists(apps_post)
+
+        self.controller.delete_app(self.app.app)
+        
+        expect_equal(found_app, True, 'find app')
+        expect_equal(len(apps_post), len(apps_pre)+1, 'num developer')
+                
+        assert_expectations()
+
+    def test_CreateAndSymbol(self):
+        # [Documentation] App - appname with '&' shall convert them to dashes (docker compliant)
+        # ... create an app with app name with '&' that is not docker compliant
+        # ... verify imagename is converted to docker compliant with '&' converted to '-'
+
+        # print the existing apps 
+        apps_pre = self.controller.show_apps()
+
+        # create the app
+        self.app = mex_controller.App(image_type=image_type,
+                                      app_name='andy&dandy',
+                                      app_version=app_version,
+                                      access_ports=access_ports,
+                                      cluster_name=cluster_name,
+                                      developer_name=developer_name,
+                                      default_flavor_name=flavor_name)
+
+        resp = self.controller.create_app(self.app.app)
+
+        # print the cluster instances after error
+        apps_post = self.controller.show_apps()
+
+        # find app in list
+        self.app.image_path = 'mobiledgex_' + developer_name + '/andy-dandy:1.0'
+        found_app = self.app.exists(apps_post)
+
+        self.controller.delete_app(self.app.app)
+        
+        expect_equal(found_app, True, 'find app')
+        expect_equal(len(apps_post), len(apps_pre)+1, 'num developer')
+                
+        assert_expectations()
+
+    def test_CreateComma(self):
+        # [Documentation] App - appname with commas shall remove them in imagepath (docker compliant)
+        # ... create an app with app name with commas that is not docker compliant
+        # ... verify imagename is converted to docker compliant with commas removed
+
+        # print the existing apps 
+        apps_pre = self.controller.show_apps()
+
+        # create the app
+        self.app = mex_controller.App(image_type=image_type,
+                                      app_name='andy,dandy',
+                                      app_version=app_version,
+                                      access_ports=access_ports,
+                                      cluster_name=cluster_name,
+                                      developer_name=developer_name,
+                                      default_flavor_name=flavor_name)
+
+        resp = self.controller.create_app(self.app.app)
+
+        # print the cluster instances after error
+        apps_post = self.controller.show_apps()
+
+        # find app in list
+        self.app.image_path = 'mobiledgex_' + developer_name + '/andydandy:1.0'
+        found_app = self.app.exists(apps_post)
+
+        self.controller.delete_app(self.app.app)
+        
+        expect_equal(found_app, True, 'find app')
+        expect_equal(len(apps_post), len(apps_pre)+1, 'num developer')
+                
+        assert_expectations()
+
+    def test_CreateBang(self):
+        # [Documentation] App - appname with '!' shall convert them to '.' (docker compliant)
+        # ... create an app with app name with '!' that is not docker compliant
+        # ... verify imagename is converted to docker compliant with '!' converted to '.'
+
+        # print the existing apps 
+        apps_pre = self.controller.show_apps()
+
+        # create the app
+        self.app = mex_controller.App(image_type=image_type,
+                                      app_name='andy!dandy',
+                                      app_version=app_version,
+                                      access_ports=access_ports,
+                                      cluster_name=cluster_name,
+                                      developer_name=developer_name,
+                                      default_flavor_name=flavor_name)
+
+        resp = self.controller.create_app(self.app.app)
+
+        # print the cluster instances after error
+        apps_post = self.controller.show_apps()
+
+        # find app in list
+        self.app.image_path = 'mobiledgex_' + developer_name + '/andy.dandy:1.0'
+        found_app = self.app.exists(apps_post)
+
+        self.controller.delete_app(self.app.app)
+        
+        expect_equal(found_app, True, 'find app')
+        expect_equal(len(apps_post), len(apps_pre)+1, 'num developer')
+                
+        assert_expectations()
+
+    @classmethod
+    def tearDownClass(self):
+        self.controller.delete_cluster(self.cluster.cluster)
+        self.controller.delete_developer(self.developer.developer)
+        self.controller.delete_cluster_flavor(self.cluster_flavor.cluster_flavor)
+        self.controller.delete_flavor(self.flavor.flavor)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(tc)
+    sys.exit(not unittest.TextTestRunner().run(suite).wasSuccessful())
+
