28797c243839ff1355b107366fb253ac66df9ade
Andy Anderson <andy.anderson@mobiledgex.com>
2019-01-16 10:03:43 -0600

    remove create dmuus cloudlet since added as standard prov  Changes to be committed: 	modified:   test_clusterDelete_beforeClusterInst.py 	modified:   test_clusterInstAdd.py 	modified:   test_clusterInstAdd_100.py 	modified:   test_clusterInstAdd_clusterNotFound.py 	modified:   test_clusterInstAdd_flavorNotExist.py 	modified:   test_clusterInstAdd_keyExists.py 	modified:   test_clusterInstAdd_liveness1.py 	modified:   test_clusterInstAdd_liveness2.py 	modified:   test_clusterInstAdd_noDefaultFlavor_noFlavor.py 	modified:   test_clusterInstAdd_noFlavor.py 	modified:   test_clusterInstDelete.py 	modified:   test_clusterInstDelete_beforeAppInst.py

--- testcases/controller/cluster/test_clusterInstDelete.py
+++ testcases/controller/cluster/test_clusterInstDelete.py
@@ -61,7 +61,7 @@ class tc(unittest.TestCase):
         #self.controller.create_operator(self.operator.operator)
         self.controller.create_flavor(self.flavor.flavor)
         self.controller.create_cluster_flavor(self.cluster_flavor.cluster_flavor)
-        self.controller.create_cloudlet(self.cloudlet.cloudlet)
+        #self.controller.create_cloudlet(self.cloudlet.cloudlet)
         self.controller.create_cluster(self.cluster.cluster)
 
     def test_DeleteClusterInstanceFlavor(self):
@@ -151,7 +151,7 @@ class tc(unittest.TestCase):
         self.controller.delete_cluster(self.cluster.cluster)
         self.controller.delete_cluster_flavor(self.cluster_flavor.cluster_flavor)
         self.controller.delete_flavor(self.flavor.flavor)
-        self.controller.delete_cloudlet(self.cloudlet.cloudlet)
+        #self.controller.delete_cloudlet(self.cloudlet.cloudlet)
         #self.controller.delete_operator(self.operator.operator)
 
 if __name__ == '__main__':
