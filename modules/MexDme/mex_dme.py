42f7c5c4fb76d18815bf97e007afbf6b06ad9e55
Andy Anderson <andy.anderson@mobiledgex.com>
2019-01-18 14:39:44 -0600

    update mex_dme.py for VerifyLocation  Changes to be committed: 	modified:   ../../../modules/MexDme/mex_dme.py

--- modules/MexDme/mex_dme.py
+++ modules/MexDme/mex_dme.py
@@ -5,6 +5,9 @@ import subprocess
 import shlex
 import logging
 import jwt
+import requests
+import subprocess
+
 from mex_grpc import MexGrpc
 
 import shared_variables
@@ -18,6 +21,8 @@ logger = logging.getLogger('mex_dme')
 
 auth_token_global = None
 session_cookie_global = None
+token_server_uri_global = None
+token_global = None
 
 class Client():
     def __init__(self, developer_name=None, app_name=None, app_version=None, auth_token=None, use_defaults=True):
@@ -106,6 +111,70 @@ class GetFqdnList():
 
         self.request = app_client_pb2.FqdnListRequest(**request_dict)
 
+class GetAppInstList():
+    def __init__(self, session_cookie=None, latitude=None, longitude=None, use_defaults=True):
+
+        request_dict = {}
+        self.session_cookie = session_cookie
+        self.latitude = latitude
+        self.longitude = longitude
+
+        if session_cookie == 'default':
+            self.session_cookie = session_cookie_global
+            
+        if use_defaults:
+            if not session_cookie: self.session_cookie = session_cookie_global
+
+        loc_dict = {}
+        if self.latitude is not None:
+            loc_dict['latitude'] = float(self.latitude)
+        if self.longitude is not None:
+            loc_dict['longitude'] = float(self.longitude)
+
+        if self.session_cookie is not None:
+            request_dict['SessionCookie'] = self.session_cookie    
+        if loc_dict:
+            request_dict['GpsLocation'] = loc_pb2.Loc(**loc_dict)
+
+        self.request = app_client_pb2.AppInstListRequest(**request_dict)
+
+class VerifyLocation():
+    def __init__(self, session_cookie=None, token=None, carrier_name=None, latitude=None, longitude=None, use_defaults=True):
+        request_dict = {}
+        self.session_cookie = session_cookie
+        self.latitude = latitude
+        self.longitude = longitude
+        self.carrier_name = carrier_name
+        self.token = token
+
+        if session_cookie == 'default':
+            self.session_cookie = session_cookie_global
+
+        if token == 'default':
+            self.token = token_global
+
+        if use_defaults:
+            if not session_cookie: self.session_cookie = session_cookie_global
+            if token is None: self.token = token_global
+
+        loc_dict = {}
+        if self.latitude is not None:
+            loc_dict['latitude'] = float(self.latitude)
+        if self.longitude is not None:
+            loc_dict['longitude'] = float(self.longitude)
+
+        if self.session_cookie is not None:
+            request_dict['SessionCookie'] = self.session_cookie
+        if self.carrier_name is not None:
+            request_dict['CarrierName'] = self.carrier_name    
+        if loc_dict:
+            request_dict['GpsLocation'] = loc_pb2.Loc(**loc_dict)
+        if self.token is not None:
+            request_dict['VerifyLocToken'] = self.token
+
+        print(request_dict)
+        self.request = app_client_pb2.VerifyLocationRequest(**request_dict)
+
 class Dme(MexGrpc):
     def __init__(self, dme_address='127.0.0.1:50051', root_cert='mex-ca.crt', key='localserver.key', client_cert='localserver.crt'):
         #self.developer_list = []
@@ -134,6 +203,7 @@ class Dme(MexGrpc):
         
     def register_client(self, register_client_obj=None, **kwargs):
         global session_cookie_global
+        global token_server_uri_global
         
         resp = None
 
@@ -150,6 +220,7 @@ class Dme(MexGrpc):
         resp = self.match_engine_stub.RegisterClient(request)
         self.session_cookie = resp.SessionCookie
         session_cookie_global = resp.SessionCookie
+        token_server_uri_global = resp.TokenServerURI
         self._token_server_uri = resp.TokenServerURI
         logger.debug('session_cookie=' + self.session_cookie)
         logger.debug('token server uri=' + self._token_server_uri)
@@ -195,8 +266,47 @@ class Dme(MexGrpc):
 
         return resp
 
-    def get_app_instance_list(self, match_engine_request_obj=None, **kwargs):
-        logger.info('get app instance list on {}. \n\t{}'.format(self.address, str(match_engine_request_obj).replace('\n','\n\t')))
+    def get_app_instance_list(self, get_app_instance_request_obj=None, **kwargs):
+        resp = None
+
+        if not get_app_instance_request_obj:
+            request = GetAppInstList(**kwargs).request
+
+        logger.info('get app instance list on {}. \n\t{}'.format(self.address, str(get_app_instance_request_obj).replace('\n','\n\t')))
+
+        resp = self.match_engine_stub.GetAppInstList(request)
+
+        if resp.Status != 1: # FL_SUCCESS
+            raise Exception('get app inst list failed:{}'.format(str(resp)))
+
+        #resp = sorted(resp.AppFqdns, key=lambda x: x.FQDN) # sorting since need to check for may apps. this return the sorted list instead of the response itself
+        #print(resp)
+
+        return resp
+
+    def verify_location(self, verify_location_request_obj=None, **kwargs):
+        resp = None
+
+        if not verify_location_request_obj:
+            request = VerifyLocation(**kwargs).request
+
+        logger.info('verify location on {}. \n\t{}'.format(self.address, str(request).replace('\n','\n\t')))
+
+        resp = self.match_engine_stub.VerifyLocation(request)
+
+        return resp
+
+    def update_location(self, latitude, longitude, ip_address=None):
+        if ip_address is None:
+            ip_address = subprocess.run('curl ifconfig.me', shell=True, check=True, capture_output=True).stdout.decode('ascii')
+
+        logger.info('updating location with ipaddr={} lat={} long={}'.format(ip_address, latitude, longitude))
+
+        location_server = 'http://mextest.locsim.mobiledgex.net:8888/updateLocation'
+        payload = '{"latitude":' + str(latitude) + ', "longitude":' + longitude + ', "ipaddr": "' + ip_address + '"}'
+        
+        requests.post(location_server, data=payload)
+            
 
     def generate_auth_token(self, app_name, app_version, developer_name, key_file='authtoken_private.pem'):
         global auth_token_global
@@ -231,7 +341,26 @@ class Dme(MexGrpc):
         auth_token_global = token
         
         return token
+
+    def get_token(self, token_server=None):
+        global token_global
         
+        if token_server is None:
+            token_server = token_server_uri_global
+            
+        logger.info('getting token from {}'.format(token_server))
+
+        resp = requests.get(token_server, allow_redirects=False)
+
+        location = resp.headers['Location']
+        logger.debug('location header:{}'.format(location))
+
+        token = location.split('dt-id=')[1]
+        token_global = token
+        logger.debug('token={}'.format(token))
+
+        return token
+    
     #def calculate_distance(latitude1=None, longitude1=None, latitude2=None, longitude2=None):
     def calculate_distance(origin=None, destination=None):
         lat1, lon1 = origin
