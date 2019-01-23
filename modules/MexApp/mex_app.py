86598208a2ef3c107e7d6e3d15c3351609cab03f
Andy Anderson <andy.anderson@mobiledgex.com>
2019-01-22 18:41:59 -0600

    update mex_app.py  Changes to be committed: 	modified:   ../../modules/MexApp/mex_app.py

--- modules/MexApp/mex_app.py
+++ modules/MexApp/mex_app.py
@@ -1,10 +1,19 @@
 import logging
 import socket
 import sys
+import os
+import subprocess
+import time
+
+import shared_variables
 
 class MexApp(object):
+    def __init__(self):
+        self.kubeconfig_dir = os.getenv('HOME') + '/.mobiledgex'
+        
     def ping_udp_port(self, host, port):
         data = 'ping'
+        exp_return_data = 'pong'
         data_size = sys.getsizeof(bytes(data, 'utf-8'))
         data_to_send = data.encode('ascii')
 
@@ -16,24 +25,29 @@ class MexApp(object):
             client_socket.sendto(data_to_send,(host, int(port)))
             (return_data, addr) = client_socket.recvfrom(data_size)
             logging.info('received this data from {}:{}'.format(addr, return_data.decode('utf-8')))
-        except:
-            e = sys.exc_info()[0]
+            client_socket.close()
+        except Exception as e:
             client_socket.close()
             raise Exception('error=', e)
             
-        if return_data.decode('utf-8') != 'ping':
-            raise Exception('correct data not received from server')
+        if return_data.decode('utf-8') != exp_return_data:
+            raise Exception('correct data not received from server. expected=' + exp_return_data + ' got=' + return_data.decode('utf-8'))
 
     def ping_tcp_port(self, host, port):
         data = 'ping'
-
+        exp_return_data = 'pong'
+        data_size = sys.getsizeof(bytes(data, 'utf-8'))
+        
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((host, int(port)))
-
+        print('cccccc')
         return_data = ''
         try:
+            logging.debug('sending data')
             client_socket.sendall(bytes(data, encoding='utf-8'))
             return_data = client_socket.recv(data_size)
+            logging.debug('data recevied back:' + return_data.decode('utf-8'))
+            client_socket.close()
         except Exception as e:
             print('caught exception')
             #print(sys.exc_info())
@@ -41,8 +55,8 @@ class MexApp(object):
             client_socket.close()
             raise Exception('error=', e)
             
-        if return_data.decode('utf-8') != 'ping':
-            raise Exception('correct data not received from server')
+        if return_data.decode('utf-8') != exp_return_data:
+            raise Exception('correct data not received from server. expected=' + exp_return_data + ' got=' + return_data.decode('utf-8'))
 
     def udp_port_should_be_alive(self, host, port):
         logging.info('host:' + host + ' port:' + str(port))
@@ -53,3 +67,24 @@ class MexApp(object):
         logging.info('host:' + host + ' port:' + str(port))
         self.ping_tcp_port(host, port)
         return True
+
+    def wait_for_k8s_pod_to_be_running(self, wait_time=600):
+        #shared_variables.cluster_name_default = 'cluster1544640562'
+        kubeconfig_file = self.kubeconfig_dir + '/' + shared_variables.cluster_name_default + '.kubeconfig'
+        logging.info('kubeconfig=' + kubeconfig_file)
+
+        kubectl_cmd = 'export KUBECONFIG={};kubectl get pods'.format(kubeconfig_file)
+        logging.info(kubectl_cmd)
+
+        for t in range(wait_time):
+            kubectl_return = subprocess.run(kubectl_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            kubectl_out = kubectl_return.stdout.decode('utf-8')
+            logging.debug(kubectl_out)
+
+            for line in kubectl_out.splitlines():
+                if line.split()[2] == 'Running':
+                    logging.info('Found running pod:' + line)
+                    return True;
+            time.sleep(1)
+
+        raise Exception('Running k8s pod not found')
