decce755f01f139319a266eb8be77756df94a79a
Andy Anderson <andy.anderson@mobiledgex.com>
2019-01-23 11:07:52 -0600

    fix testcase failure and update mex_controlle for returning app after create_app  Changes to be committed: 	modified:   ../../../modules/MexController/mex_controller.py 	modified:   ../app/test_appAdd_IpAccessDedicatedOrSharedMulti.py 	modified:   updateCloutlet_fail.robot

--- testcases/controller/app/test_appAdd_IpAccessDedicatedOrSharedMulti.py
+++ testcases/controller/app/test_appAdd_IpAccessDedicatedOrSharedMulti.py
@@ -188,7 +188,7 @@ class tc(unittest.TestCase):
 
         self.controller.delete_app(self.app.app)
 
-        port_match = 'ports:\n        - containerPort: 1\n          protocol: TCP\n        - containerPort: 1\n          protocol: UDP\n        - containerPort: 2\n          protocol: TCP\n        - containerPort: 2\n          protocol: UDP\n        - containerPort: 3\n          protocol: TCP\n        - containerPort: 3\n          protocol: UDP'
+        port_match = 'ports:\n        - containerPort: 1\n          protocol: TCP\n        - containerPort: 1\n          protocol: UDP\n        - containerPort: 2\n          protocol: TCP\n        - containerPort: 2\n          protocol: UDP\n        - containerPort: 3\n          protocol: UDP\n        - containerPort: 3\n          protocol: TCP'
         expect(port_match in resp.deployment_manifest, 'manifest ports')
         
         expect_equal(found_app, True, 'find app')
