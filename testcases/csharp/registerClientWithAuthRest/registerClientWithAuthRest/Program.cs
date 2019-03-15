a5d85fb31cc933ae65cc02e2268b3f79c06592af
Leon Adams <leon.adams@mobiledgex.com>
2019-03-15 15:21:29 -0500

    Readding this testcase  Changes to be committed: 	new file:   registerClientWithAuthRest/.gitignore 	new file:   registerClientWithAuthRest/registerClientWithAuthRest.sln 	new file:   registerClientWithAuthRest/registerClientWithAuthRest/Program.cs 	new file:   registerClientWithAuthRest/registerClientWithAuthRest/registerClientWithAuthRest.csproj

---
+++ testcases/csharp/registerClientWithAuthRest/registerClientWithAuthRest/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Threading.Tasks;
+using DistributedMatchEngine;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace RestSample
+{
+    class Program
+    {
+        static string tokenServerURI = "http://mextest.tok.mobiledgex.net:9999/its?followURL=https://dme.mobiledgex.net/verifyLoc";
+        static string carrierName = "tmus";
+        //static string appName = "EmptyMatchEngineApp";
+        //static string devName = "EmptyMatchEngineApp";
+        static string devName = "automation_api";
+        static string appName = "automation_api_auth_app";
+        static string appVers = "1.0";
+        static string developerAuthToken = "";
+
+        //static string host = "tdg.dme.mobiledgex.net";
+        static string host = "automationbonn.dme.mobiledgex.net";
+        static UInt32 port = 38001;
+        static string sessionCookie;
+
+        // Get the ephemerial carriername from device specific properties.
+        async static Task<string> getCurrentCarrierName()
+        {
+            var dummy = await Task.FromResult(0);
+            return carrierName;
+        }
+
+        async static Task Main(string[] args)
+        {
+            try
+            {
+                carrierName = await getCurrentCarrierName();
+
+                Console.WriteLine("RegisterClientWrongAppRest Testcase");
+
+                MatchingEngine me = new MatchingEngine();
+                //port = MatchingEngine.defaultDmeRestPort;
+
+                // Generate the authToken
+                //var pubkey = "/home/jenkins/go/src/github.com/mobiledgex/edge-cloud-qa/certs/authtoken_private.pem";
+                var pubkey = "/Users/leon.adams/go/src/github.com/mobiledgex/edge-cloud-qa/certs/authtoken_private.pem";
+                System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo("genauthtoken");
+                psi.Arguments = "-appname automation_api_auth_app -appvers 1.0 -devname automation_api -privkeyfile " + pubkey;
+                psi.RedirectStandardOutput = true;
+                System.Diagnostics.Process genauthtoken;
+                genauthtoken = System.Diagnostics.Process.Start(psi);
+                genauthtoken.WaitForExit();
+                System.IO.StreamReader reader = genauthtoken.StandardOutput;
+                genauthtoken.WaitForExit();
+                if (genauthtoken.HasExited)
+                {
+                    developerAuthToken = reader.ReadToEnd();
+                }
+                developerAuthToken = developerAuthToken.Substring(6);
+                developerAuthToken = developerAuthToken.Trim();
+                //Console.WriteLine(developerAuthToken);
+                //developerAuthToken = "";
+
+                // Start location task:
+                var locTask = Util.GetLocationFromDevice();
+
+                var registerClientRequest = me.CreateRegisterClientRequest(carrierName, devName, appName, appVers, developerAuthToken);
+
+                // Await synchronously.
+                //Console.WriteLine("Port: " + port);
+                var registerClientReply = await me.RegisterClient(host, port, registerClientRequest);
+
+
+                //var loc = await locTask;
+                long timeLongMs = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
+                long seconds = timeLongMs / 1000;
+                int nanoSec = (int)(timeLongMs % 1000) * 1000000;
+                var ts = new Timestamp { nanos = nanoSec, seconds = seconds };
+                var loc = new Loc()
+                {
+                    course = 0,
+                    altitude = 100,
+                    horizontal_accuracy = 5,
+                    speed = 2,
+                    longitude = 13.405,
+                    latitude = 52.52,
+                    vertical_accuracy = 20,
+                    timestamp = ts
+                };
+
+                //Verify the Token Server URI is correct
+                if (registerClientReply.TokenServerURI != tokenServerURI)
+                {
+                    Environment.Exit(1);
+                }
+                else
+                {
+                    Console.WriteLine("\n\nToken Server URI Correct!");
+                }
+
+                // Store sessionCookie, for later use in future requests.
+                sessionCookie = registerClientReply.SessionCookie;
+
+                //Setup to handle the sessiontoken
+                var jwtHandler = new JwtSecurityTokenHandler();
+                System.IdentityModel.Tokens.Jwt.JwtSecurityToken secToken = null;
+                secToken = jwtHandler.ReadJwtToken(sessionCookie);
+                var claims = secToken.Claims;
+                var jwtPayload = "";
+                foreach (Claim c in claims)
+                {
+                    jwtPayload += '"' + c.Type + "\":\"" + c.Value + "\",";
+                }
+
+
+                //Extract the sessiontoken contents
+                char[] delimiterChars = { ',', '{', '}' };
+                string[] words = jwtPayload.Split(delimiterChars);
+                long expTime = 0;
+                long iatTime = 0;
+                bool expParse = false;
+                bool iatParse = false;
+                string peer;
+                string dev;
+                string app;
+                string appver;
+
+                foreach (var word in words)
+                {
+                    if (word.Length > 7)
+                    {
+                        //Console.WriteLine(word);
+                        if (word.Substring(1, 3) == "exp")
+                        {
+                            expParse = long.TryParse(word.Substring(7, 10), out expTime);
+                        }
+                        if (word.Substring(1, 3) == "iat")
+                        {
+                            iatParse = long.TryParse(word.Substring(7, 10), out iatTime);
+                        }
+                        if (expParse && iatParse)
+                        {
+                            int divider = 60;
+                            long tokenTime = expTime - iatTime;
+                            tokenTime /= divider;
+                            tokenTime /= divider;
+                            int expLen = 24;
+                            expParse = false;
+                            if (tokenTime != expLen)
+                            {
+                                Console.WriteLine("Session Cookie Exparation Time not 24 Hours:  " + tokenTime);
+                                Environment.Exit(1);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Session Cookie Exparation Time correct:  " + tokenTime);
+                            }
+                        }
+                        if (word.Substring(1, 6) == "peerip")
+                        {
+                            peer = word.Substring(10);
+                            peer = peer.Substring(0, peer.Length - 1);
+                            string pattern = "^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$";
+                            if (System.Text.RegularExpressions.Regex.IsMatch(peer, pattern))
+                            {
+                                Console.WriteLine("Peerip Expression Matched!! " + peer);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Peerip Expression Didn't Match!  " + peer);
+                                Environment.Exit(1);
+                            }
+                        }
+                        if (word.Substring(1, 7) == "devname")
+                        {
+                            dev = word.Substring(11);
+                            dev = dev.Substring(0, dev.Length - 1);
+                            if (dev != devName)
+                            {
+                                Console.WriteLine("Devname Didn't Match!  " + dev);
+                                Environment.Exit(1);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Devname Matched!  " + dev);
+                            }
+                        }
+                        if (word.Substring(1, 7) == "appname")
+                        {
+                            app = word.Substring(11);
+                            app = app.Substring(0, app.Length - 1);
+                            if (app != appName)
+                            {
+                                Console.WriteLine("AppName Didn't Match!  " + app);
+                                Environment.Exit(1);
+                            }
+                            else
+                            {
+                                Console.WriteLine("AppName Matched!  " + app);
+                            }
+                        }
+                        if (word.Substring(1, 7) == "appvers")
+                        {
+                            appver = word.Substring(11, 3);
+                            if (appver != "1.0")
+                            {
+                                Console.WriteLine("App Version Didn't Match!  " + appver);
+                                Environment.Exit(1);
+                            }
+                            else
+                            {
+                                Console.WriteLine("App Version Matched!  " + appver);
+                            }
+                        }
+
+                    }
+                    // Console.WriteLine("App Version Matched!  " + appver);
+                }
+                Console.WriteLine("Test Case Passed!!!");
+            }
+            catch (InvalidTokenServerTokenException itste)
+            {
+                Console.WriteLine(itste.StackTrace);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    };
+}
