ae20a5a526656359aed0cb355503b24a8797f1d4
Leon Adams <leon.adams@mobiledgex.com>
2019-05-15 20:14:38 -0500

    Updated the CSharp README file and added findCloudletBadSessionCookie/Rest test cases  Changes to be committed: 	modified:   csharp/README.md 	new file:   csharp/findCloudletBadSessionCookie/findCloudletBadSessionCookie.sln 	new file:   csharp/findCloudletBadSessionCookie/findCloudletBadSessionCookie/Program.cs 	new file:   csharp/findCloudletBadSessionCookie/findCloudletBadSessionCookie/findCloudletBadSessionCookie.csproj 	new file:   csharp/findCloudletBadSessionCookieRest/findCloudletBadSessionCookieRest.sln 	new file:   csharp/findCloudletBadSessionCookieRest/findCloudletBadSessionCookieRest/Program.cs 	new file:   csharp/findCloudletBadSessionCookieRest/findCloudletBadSessionCookieRest/findCloudletBadSessionCookieRest.csproj

---
+++ testcases/csharp/findCloudletBadSessionCookieRest/findCloudletBadSessionCookieRest/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Threading.Tasks;
+using DistributedMatchEngine;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace RestSample
+{
+    class Program
+    {
+        static string tokenServerURI = "http://mextest.tok.mobiledgex.net:9999/its?followURL=https://dme.mobiledgex.net/verifyLoc";
+        static string carrierName = "tmus";
+        //static string appName = "EmptyMatchEngineApp";
+        //static string devName = "EmptyMatchEngineApp";
+        static string devName = "automation_api";
+        static string appName = "automation_api_app";
+        static string appVers = "1.0";
+        static string developerAuthToken = "";
+
+        //static string host = "tdg.dme.mobiledgex.net";
+        static string host = "automationbonn.dme.mobiledgex.net";
+        static UInt32 port = 38001;
+        static string sessionCookie;
+
+        // Get the ephemerial carriername from device specific properties.
+        async static Task<string> getCurrentCarrierName()
+        {
+            var dummy = await Task.FromResult(0);
+            return carrierName;
+        }
+
+        async static Task Main(string[] args)
+        {
+            try
+            {
+                carrierName = await getCurrentCarrierName();
+
+                Console.WriteLine("FindCloudletBadSessionCookieRest Testcase");
+
+                MatchingEngine me = new MatchingEngine();
+                //port = MatchingEngine.defaultDmeRestPort;
+
+                // Start location task:
+                var locTask = Util.GetLocationFromDevice();
+
+                var registerClientRequest = me.CreateRegisterClientRequest(carrierName, devName, appName, appVers, developerAuthToken);
+
+                // Await synchronously.
+                //Console.WriteLine("Port: " + port);
+                var registerClientReply = await me.RegisterClient(host, port, registerClientRequest);
+
+
+                //var loc = await locTask;
+                long timeLongMs = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
+                long seconds = timeLongMs / 1000;
+                int nanoSec = (int)(timeLongMs % 1000) * 1000000;
+                var ts = new Timestamp { nanos = nanoSec, seconds = seconds };
+                var loc = new Loc()
+                {
+                    course = 0,
+                    altitude = 100,
+                    horizontal_accuracy = 5,
+                    speed = 2,
+                    longitude = 13.405,
+                    latitude = 52.52,
+                    vertical_accuracy = 20,
+                    timestamp = ts
+                };
+
+                //Verify the Token Server URI is correct
+                if (registerClientReply.TokenServerURI != tokenServerURI)
+                {
+                    Environment.Exit(1);
+                }
+                else
+                {
+                    Console.WriteLine("Token Server URI Correct!");
+                }
+
+                // Store sessionCookie, for later use in future requests.
+                sessionCookie = registerClientReply.SessionCookie;
+
+                me.sessionCookie = "XX";
+                //Console.WriteLine("sessionCookie = " + sessionCookie);
+
+                var findCloudletRequest = me.CreateFindCloudletRequest(carrierName, devName, appName, appVers, loc);
+
+                // Async:
+                var findCloudletTask = me.FindCloudlet(host, port, findCloudletRequest);
+
+                // Awaits:
+                var findCloudletReply = await findCloudletTask;
+                if (findCloudletReply.status.ToString() == "FIND_FOUND")
+                {
+                    Console.WriteLine("FindCloudlet Reply: " + findCloudletReply.status);
+                    Console.WriteLine("FindCloudlet Reply: " + findCloudletReply.FQDN);
+                    Console.WriteLine("FindCloudlet Reply: " + findCloudletReply.cloudlet_location.latitude);
+                    Console.WriteLine("FindCloudlet Reply: " + findCloudletReply.cloudlet_location.longitude);
+                    Console.WriteLine("Test Case Failed!!!");
+                    Environment.Exit(1);
+                }
+                if (findCloudletReply.status.ToString() == "FIND_NOTFOUND")
+                {
+                    Console.WriteLine("FindCloudlet Reply: " + findCloudletReply.status);
+                    Console.WriteLine("Test Case Failed!!!");
+                    Environment.Exit(1);
+                }
+            }
+            catch (InvalidTokenServerTokenException itste)
+            {
+                Console.WriteLine(itste.StackTrace);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error Message: " + e.Message);
+                Console.WriteLine("Test Case Passed!");
+                Environment.Exit(0);
+            }
+
+        }
+    };
+}
