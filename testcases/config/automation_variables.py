9a64178f5df74cb29fb49b5e2a4ac22525e4a3d1
Andy Anderson <andy.anderson@mobiledgex.com>
2019-05-16 08:46:22 -0500

    change to Hamburg  Changes to be committed: 	modified:   ../config/automation_variables.py

--- testcases/config/automation_variables.py
+++ testcases/config/automation_variables.py
@@ -1,3 +1,3 @@
 cloudlet_name_azure = 'automationAzureCentralCloudlet'
-cloudlet_name_openstack = 'automationBonnCloudlet'
+cloudlet_name_openstack = 'automationHamburgCloudlet'
 docker_image = 'docker.mobiledgex.net/mobiledgex/images/server_ping_threaded:5.0'
