4fb6601427d621c16e245d00f7ad5193aa7b6c8d
Andy Anderson <andy.anderson@mobiledgex.com>
2019-05-18 16:29:07 -0500

    remove cluster_flavor from app testscases  Changes to be committed: 	modified:   test_appAdd_100.py 	modified:   test_appAdd_IpAccessDedicated65535.py 	modified:   test_appAdd_IpAccessDedicatedOrSharedMulti.py 	modified:   test_appAdd_IpAccessDedicatedOrSharedPort1.py 	modified:   test_appAdd_IpAccessDedicatedOrSharedPort65535.py 	modified:   test_appAdd_IpAccessDedicatedPort1.py 	modified:   test_appAdd_IpAccessDedicatedPortMulti.py 	modified:   test_appAdd_IpAccessSharedMulti.py 	modified:   test_appAdd_IpAccessSharedPort1.py 	modified:   test_appAdd_IpAccessSharedPort65535.py 	modified:   test_appAdd_appNameEmpty.py 	modified:   test_appAdd_config.py 	modified:   test_appAdd_config_invalid.py 	modified:   test_appAdd_docker.py 	modified:   test_appAdd_dockerCompliant.py 	modified:   test_appAdd_emptyCluster.py 	modified:   test_appAdd_imagePath.py 	modified:   test_appAdd_keyExists.py 	modified:   test_appAdd_noIpAccess.py 	modified:   test_appAdd_noPorts.py 	modified:   test_appAdd_qcow.py 	modified:   test_appDelete_appInstExists.py 	modified:   test_appDelete_keyNotFound.py 	modified:   test_appShow_queryParms.py

--- testcases/controller/app/test_appAdd_IpAccessDedicatedOrSharedPort1.py
+++ testcases/controller/app/test_appAdd_IpAccessDedicatedOrSharedPort1.py
@@ -42,7 +42,6 @@ class tc(unittest.TestCase):
                                                     client_cert = mex_cert
                                                    )
         self.flavor = mex_controller.Flavor(flavor_name=flavor_name, ram=1024, vcpus=1, disk=1)
-        self.cluster_flavor = mex_controller.ClusterFlavor(cluster_flavor_name=flavor_name, node_flavor_name=flavor_name, master_flavor_name=flavor_name, number_nodes=1, max_nodes=1, number_masters=1)
 
         self.developer = mex_controller.Developer(developer_name=developer_name,
                                                   developer_address=developer_address,
@@ -51,7 +50,6 @@ class tc(unittest.TestCase):
                                               default_flavor_name=flavor_name)
 
         self.controller.create_flavor(self.flavor.flavor)
-        self.controller.create_cluster_flavor(self.cluster_flavor.cluster_flavor)
         self.controller.create_developer(self.developer.developer) 
         self.controller.create_cluster(self.cluster.cluster)
 
@@ -484,7 +482,6 @@ class tc(unittest.TestCase):
     def tearDownClass(self):
         self.controller.delete_cluster(self.cluster.cluster)
         self.controller.delete_developer(self.developer.developer)
-        self.controller.delete_cluster_flavor(self.cluster_flavor.cluster_flavor)
         self.controller.delete_flavor(self.flavor.flavor)
 
 if __name__ == '__main__':
