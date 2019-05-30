4b76a2ba765263519463f365bfda07540148eaa1
Andy Anderson <andy.anderson@mobiledgex.com>
2019-05-29 19:50:48 -0500

    add console stuff  Changes to be committed: 	modified:   ../../modules/MexConsole.py 	modified:   ../../modules/MexMasterController.py 	modified:   ../../modules/console/base_page.py 	new file:   ../../modules/console/cloudlets_page.py 	modified:   ../../modules/console/compute_page.py 	new file:   ../../modules/console/flavors_page.py 	modified:   ../../modules/console/locators.py 	new file:   ../../modules/console/new_cloudlet_settings_page.py 	new file:   ../../modules/console/new_settings_page.py 	modified:   ../../modules/mex_rest.py 	new file:   flavor_create.robot 	new file:   flavor_show.robot 	modified:   ../controller/app/createAppInst.robot

--- modules/MexConsole.py
+++ modules/MexConsole.py
@@ -6,7 +6,10 @@ import time
 from console.login_page import LoginPage
 from console.main_page import MainPage
 from console.compute_page import ComputePage
+from console.flavors_page import FlavorsPage
+from console.cloudlets_page import CloudletsPage
 from console.new_settings_page import NewFlavorSettingsPage
+from console.new_cloudlet_settings_page import NewCloudletSettingsPage
 
 from mex_controller_classes import Flavor
 
@@ -20,6 +23,11 @@ class MexConsole() :
         print('*WARN*', 'INIT')
         self.url = url
         self.username = None
+
+        self._region = None
+        self._flavor = None
+        self._cloudlet = None
+        
         chrome_options = Options()
         #chrome_options.add_argument("--disable-extensions")
         #chrome_options.add_argument("--disable-gpu")
@@ -30,7 +38,10 @@ class MexConsole() :
         self.driver.get(url)
 
         self.compute_page = ComputePage(self.driver)
+        self.flavors_page = FlavorsPage(self.driver)
+        self.cloudlets_page = CloudletsPage(self.driver)
         self.new_flavor_page = NewFlavorSettingsPage(self.driver)
+        self.new_cloudlet_page = NewCloudletSettingsPage(self.driver)
         
     def login_to_mex_console(self, browser='Chrome', username='mexadmin', password='mexadmin123'):
         self.take_screenshot('loginpage_pre')
@@ -82,16 +93,18 @@ class MexConsole() :
         main_page = MainPage(self.driver)
         main_page.click_compute_button()
 
+        self.take_screenshot('compute_clicked')
+        
         compute_page = ComputePage(self.driver)
         if compute_page.is_branding_present():
             logging.info('branding present')
         else:
             raise Exception('Branding not present')
 
-        if compute_page.is_refresh_icon_present():
-            logging.info('refresh icon present')
-        else:
-            raise Exception('Refresh icon not present')
+        #if compute_page.is_refresh_icon_present():
+        #    logging.info('refresh icon present')
+        #else:
+        #    raise Exception('Refresh icon not present')
 
         if compute_page.is_public_icon_present():
             logging.info('public icon present')
@@ -148,7 +161,7 @@ class MexConsole() :
         else:
             raise Exception('flavor heading not present')
 
-        if self.compute_page.is_flavor_table_header_present():
+        if self.flavors_page.is_flavors_table_header_present():
             logging.info('flavor table header present')
         else:
             raise Exception('flavor header not present')
@@ -156,7 +169,26 @@ class MexConsole() :
         time.sleep(3)  # wait for table to load. maybe a better way to do this
 
         self.take_screenshot('open_flavors_post')
-        
+
+    def open_cloudlets(self):
+        self.take_screenshot('open_cloudlets_pre')
+
+        self.compute_page.click_cloudlets()
+
+        if self.compute_page.is_table_heading_present('Cloudlets'):
+            logging.info('cloudlet heading present')
+        else:
+            raise Exception('cloudlet heading not present')
+
+        if self.cloudlets_page.is_cloudlets_table_header_present():
+            logging.info('cloudlet table header present')
+        else:
+            raise Exception('cloudlet header not present')
+
+        time.sleep(3)  # wait for table to load. maybe a better way to do this
+
+        self.take_screenshot('open_cloudlets_post')
+
     def add_new_flavor(self, region=None, flavor_name=None, ram=None, vcpus=None, disk=None):
         self.take_screenshot('add_new_flavor_pre')
 
@@ -168,24 +200,75 @@ class MexConsole() :
             raise Exception('click New Flavor button verifcation failed')
 
         flavor = Flavor(flavor_name=flavor_name, ram=ram, vcpus=vcpus, disk=disk).flavor
+        self._flavor = flavor
+        self._region = region
         print('*WARN*', 'flavor', flavor)
 
-        self.new_flavor_page.region = 'US'
-        
+        self.new_flavor_page.create_flavor(region, flavor['key']['name'], flavor['ram'], flavor['vcpus'], flavor['disk'])
+              
         self.take_screenshot('add_new_flavor_post')
+
+    def add_new_cloudlet(self, region=None, flavor_name=None, ram=None, vcpus=None, disk=None):
+        self.take_screenshot('add_new_cloudlet_pre')
+
+        self.compute_page.click_new_button()
+
+        if self.new_cloudlet_page.are_elements_present():
+            logging.info('click New Cloudlet button verification succeeded')
+        else:
+            raise Exception('click New Cloudlet button verifcation failed')
+
+        cloudlet = Cloudlet(flavor_name=flavor_name, ram=ram, vcpus=vcpus, disk=disk).cloudlet
+        self._cloudlet = cloudlet
+        self._region = region
+        print('*WARN*', 'cloudlet', cloudlet)
+
+        self.new_cloudlet_page.create_cloudlet(region, flavor['key']['name'], flavor['ram'], flavor['vcpus'], flavor['disk'])
+              
+        self.take_screenshot('add_new_cloudlet_post')
+
+    def flavor_should_exist(self, region=None, flavor_name=None, ram=None, vcpus=None, disk=None, wait=5):
+        self.take_screenshot('flavor_should_exist_pre')
+        logging.info(f'flavor_should_exist region={region} flavor={flavor_name} ram={ram} vcpus={vcpus} disk={disk} wait={wait}')
         
+        if region is None: region = self._region
+        if flavor_name is None: flavor_name = self._flavor['key']['name']
+        if ram is None: ram = self._flavor['ram']
+        if vcpus is None: vcpus = self._flavor['vcpus']
+        if disk is None: disk = self._flavor['disk']
+        
+        if self.flavors_page.wait_for_flavor(region, flavor_name, ram, vcpus, disk):
+            logging.info('flavor found')
+        else:
+            raise Exception('Flavor NOT found')
+            
+        #rows = self.get_table_data()
+        #print('*WARN*', 'sf', self._flavor)
+        #for r in rows:
+        #    print('*WARN*', 'flavorr', r)
+        #    if r[0] == region and r[1] == 'flavor_name':
+        #        print('*WARN*', 'found flavor')
+        #        return True
+
+        #return False
+            
+    #def flavor_should
     def get_table_data(self):
         self.take_screenshot('get_table_data_pre')
 
         rows = self.compute_page.get_table_rows()
-        for r in rows:
-            print('*WARN*', 'r', r)
+        #for r in rows:
+        #    print('*WARN*', 'r', r)
 
+        return rows
+    
     def take_screenshot(self, name):
-        self.driver.save_screenshot(name+'.png')
-        logger.info(f'<img src="{name}.png">', html=True)
+        self.compute_page.take_screenshot(name + '.png')
+        #self.driver.save_screenshot(name+'.png')
+        #logger.info(f'<img src="{name}.png">', html=True)
         
     def close_browser(self):
-        self.take_screenshot('closebrowser')
+        #self.take_screenshot('closebrowser')
+        self.compute_page.take_screenshot('closebrowser.png')
         self.driver.close()
         
