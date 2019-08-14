7c0f2222d852eaea656322ad229c16cbbfcf0a6e
Andy Anderson <andy.anderson@mobiledgex.com>
2019-08-14 14:43:30 -0600

    update app tests for ports prepended with 0  Changes to be committed: 	modified:   test_appAdd_IpAccessDedicatedOrSharedPort1.py 	modified:   test_appAdd_IpAccessDedicatedOrSharedPort65535.py 	modified:   test_appAdd_IpAccessDedicatedPort1.py

--- testcases/controller/app/test_appAdd_IpAccessDedicatedOrSharedPort1.py
+++ testcases/controller/app/test_appAdd_IpAccessDedicatedOrSharedPort1.py
@@ -116,7 +116,7 @@ class tc(unittest.TestCase):
 
         self.controller.delete_app(self.app.app)
        
-        port_match = 'ports:\n        - containerPort: 01\n          protocol: TCP'
+        port_match = 'ports:\n        - containerPort: 1\n          protocol: TCP'
         expect(port_match in resp.deployment_manifest, 'manifest ports')
  
         expect_equal(found_app, True, 'find app')
@@ -184,7 +184,7 @@ class tc(unittest.TestCase):
 
         self.controller.delete_app(self.app.app)
        
-        port_match = 'ports:\n        - containerPort: 01\n          protocol: UDP'
+        port_match = 'ports:\n        - containerPort: 1\n          protocol: UDP'
         expect(port_match in resp.deployment_manifest, 'manifest ports')
  
         expect_equal(found_app, True, 'find app')
@@ -256,7 +256,7 @@ class tc(unittest.TestCase):
 
         self.controller.delete_app(self.app.app)
        
-        port_match = 'ports:\n        - containerPort: 01\n          protocol: TCP'
+        port_match = 'ports:\n        - containerPort: 1\n          protocol: TCP'
         expect(port_match in resp.deployment_manifest, 'manifest ports')
  
         expect_equal(found_app, True, 'find app')
