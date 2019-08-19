697f78ef1eb5890214c104874447203ed1a7e448
Andy Anderson <andy.anderson@mobiledgex.com>
2019-08-19 14:51:58 -0600

    fix controller specific tests  Changes to be committed: 	modified:   test_showController_addr.py 	modified:   test_showController_all.py

--- testcases/controller/controller/test_showController_all.py
+++ testcases/controller/controller/test_showController_all.py
@@ -10,6 +10,7 @@ import sys
 from delayedassert import expect, expect_equal, assert_expectations
 import logging
 import os
+import re
 
 import MexController as mex_controller
 
@@ -42,9 +43,61 @@ class tc(unittest.TestCase):
         # show controllers
         resp = self.controller.show_controllers()
 
-        #expect_equal(len(resp), 2, 'number of controllers')
-        expect_equal(len(resp), 1, 'number of controllers')
-        expect_equal(resp[0].key.addr, controller1_address, 'addr 1')
+#   ${build_head}=  Catenate  SEPARATOR=  ${nodes[0]['data']['build_master']}  +
+#   Should Match Regexp             ${nodes[0]['data']['key']['name']}  \\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b:55001
+#   Should Be Equal As Integers     ${nodes[0]['data']['key']['node_type']}  3
+#   Should Be Equal As Integers     ${op_len}  0
+#   Should Match Regexp             ${nodes[0]['data']['build_master']}  v\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\-\\d{1,3}-\\b
+#   Should Be Equal                 ${nodes[0]['data']['build_head']}  ${build_head}
+#   Should Match Regexp             ${nodes[0]['data']['hostname']}  ^controller-
+
+#- key:
+#    addr: 10.12.0.207:55001
+#  buildmaster: v1.0.2-4-gab9d724
+#  buildhead: v1.0.2-4-gab9d724+
+#  hostname: controller-654688f58-chsrk
+
+        re_master = re.compile('v\d{1,3}\.\d{0,3}\.\d{1,3}\-\d{1,9}-*')
+        re_host = re.compile('^controller-')
+        re_ip = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}')
+
+        build_head0 = resp[0].build_master + '+'
+        build_head1 = resp[1].build_master + '+'
+
+        foundhead0 = False
+        foundhost0 = False
+        foundip0 = False
+        foundhead1 = False
+        foundhost1 = False
+        foundip1 = False
+
+        if re_master.match(resp[0].build_master):
+           foundhead0=True
+        if re_host.match(resp[0].hostname):
+           foundhost0=True
+        if re_ip.match(resp[0].key.addr):
+           foundip0=True
+
+        if re_master.match(resp[1].build_master):
+           foundhead1=True
+        if re_host.match(resp[1].hostname):
+           foundhost1=True
+        if re_ip.match(resp[1].key.addr):
+           foundip1=True
+ 
+        expect_equal(len(resp), 2, 'number of controllers')
+        expect_equal(foundhead0, True, 'buildmaster')
+        expect_equal(foundhost0, True, 'host')
+        expect_equal(foundip0, True, 'host')
+        expect_equal(resp[0].build_head, build_head0, 'buildhead0')
+
+        expect_equal(foundhead1, True, 'buildmaster')
+        expect_equal(foundhost1, True, 'host')
+        expect_equal(foundip1, True, 'host')
+        expect_equal(resp[1].build_head, build_head1, 'buildhead1')
+
+        #expect_equal(len(resp), 1, 'number of controllers')
+        #expect_equal(resp[0].key.addr, controller1_address, 'addr 1')
         #expect_equal(resp[1].key.addr, controller2_address, 'addr 1')
 
         assert_expectations()
