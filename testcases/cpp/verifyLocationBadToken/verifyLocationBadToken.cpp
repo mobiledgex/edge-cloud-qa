a4fa812e738942ca770191e12cca82867bc476ff
Leon Adams <leon.adams@mobiledgex.com>
2019-08-27 19:32:37 -0500

    Updated C++ Makefiles and c++ files for changes in the code  Changes to be committed: 	modified:   cpp/findCloudletNoSessionCookie/Makefile 	modified:   cpp/findCloudletNoSessionCookie/findCloudletNoSessionCookie.cpp 	modified:   cpp/findCloudletSuccess/Makefile 	modified:   cpp/findCloudletSuccess/findCloudletSuccess.cpp 	modified:   cpp/registerClient/Makefile 	modified:   cpp/registerClient/registerClient.cpp 	modified:   cpp/registerClientWithAuth/Makefile 	modified:   cpp/registerClientWithAuth/registerClientWithAuth.cpp 	modified:   cpp/registerClientWithAuthWrongApp/Makefile 	modified:   cpp/registerClientWithAuthWrongApp/registerClientWithAuthWrongApp.cpp 	modified:   cpp/registerClientWithAuthWrongAppVer/Makefile 	modified:   cpp/registerClientWithAuthWrongAppVer/registerClientWithAuthWrongAppVer.cpp 	modified:   cpp/registerClientWithAuthWrongDev/Makefile 	modified:   cpp/registerClientWithAuthWrongDev/registerClientWithAuthWrongDev.cpp 	modified:   cpp/registerClientWrongApp/Makefile         modified:   cpp/registerClientWrongApp/registerClientWrongApp.cpp 	modified:   cpp/registerClientWrongAppVersion/Makefile 	modified:   cpp/registerClientWrongAppVersion/registerClientWrongAppVersion.cpp 	modified:   cpp/registerClientWrongDevName/Makefile 	modified:   cpp/registerClientWrongDevName/registerClientWrongDevName.cpp 	modified:   cpp/verifyLocation/Makefile 	modified:   cpp/verifyLocation/verifyLocation.cpp 	modified:   cpp/verifyLocation100KM/Makefile 	modified:   cpp/verifyLocation100KM/verifyLocation100KM.cpp 	modified:   cpp/verifyLocation10KM/Makefile 	modified:   cpp/verifyLocation10KM/verifyLocation10KM.cpp 	modified:   cpp/verifyLocationBadCarrier/Makefile 	modified:   cpp/verifyLocationBadCarrier/verifyLocationBadCarrier.cpp 	modified:   cpp/verifyLocationBadCookie/Makefile 	modified:   cpp/verifyLocationBadCookie/verifyLocationBadCookie.cpp 	modified:   cpp/verifyLocationBadToken/Makefile 	modified:   cpp/verifyLocationBadToken/verifyLocationBadToken.cpp 	modified:   cpp/verifyLocationMisMatchOtherCountry/Makefile 	modified:   cpp/verifyLocationMisMatchOtherCountry/verifyLocationMisMatchOtherCountry.cpp 	modified:   cpp/verifyLocationMisMatchSameCountry/Makefile 	modified:   cpp/verifyLocationMisMatchSameCountry/verifyLocationMisMatchSameCountry.cpp 	modified:   cpp/verifyLocationRoamingCountryMatch/Makefile 	modified:   cpp/verifyLocationRoamingCountryMatch/verifyLocationRoamingCountryMatch.cpp 	modified:   cpp/verifyLocationRoamingCountryMisMatch/Makefile 	modified:   cpp/verifyLocationRoamingCountryMisMatch/verifyLocationRoamingCountryMisMatch.cpp

--- testcases/cpp/verifyLocationBadToken/verifyLocationBadToken.cpp
+++ testcases/cpp/verifyLocationBadToken/verifyLocationBadToken.cpp
@@ -1,12 +1,13 @@
 #include <grpcpp/grpcpp.h>
 #include <iostream>
+#include <sstream>
+#include <fstream>
 #include <string>
 #include <regex>
 
 #include <curl/curl.h>
 
 #include "app-client.grpc.pb.h"
-#include "test_credentials.hpp"
 #include "jwt.h"
 
 using namespace std;
@@ -18,13 +19,6 @@ using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
 
-
-// Test Cert files:
-struct MutualAuthFiles {
-    const string caCrtFile = "../../../certs/mex-ca.crt";
-    const string clientCrtFile = "../../../certs/mex-client.crt";
-    const string clientKeyFile = "../../../certs/mex-client.key";
-} mutualAuthFiles;
 
 class MexGrpcClient {
   public:
@@ -198,12 +192,6 @@ class MexGrpcClient {
         curl_easy_setopt(curl, CURLOPT_HEADERDATA, &(this->token));
         curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header_callback);
 
-        // SSL Setup:
-        curl_easy_setopt(curl, CURLOPT_SSLCERT, mutualAuthFiles.clientCrtFile.c_str());
-        curl_easy_setopt(curl, CURLOPT_SSLKEY, mutualAuthFiles.clientKeyFile.c_str());
-        // CA:
-        curl_easy_setopt(curl, CURLOPT_CAINFO, mutualAuthFiles.caCrtFile.c_str());
-
         // verify peer or disconnect
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
 
@@ -269,17 +257,6 @@ class MexGrpcClient {
       // Set the output to null
       curl_easy_setopt(curl, CURLOPT_WRITEDATA, wfd);
       curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
-
-      
-      // Set return pointer (the token), for the header callback.
-      //curl_easy_setopt(curl, CURLOPT_HEADERDATA, &(this->token));
-      //curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header_callback);
-      
-      // SSL Setup:
-      curl_easy_setopt(curl, CURLOPT_SSLCERT, mutualAuthFiles.clientCrtFile.c_str());
-      curl_easy_setopt(curl, CURLOPT_SSLKEY, mutualAuthFiles.clientKeyFile.c_str());
-      // CA:
-      curl_easy_setopt(curl, CURLOPT_CAINFO, mutualAuthFiles.caCrtFile.c_str());
       
       // verify peer or disconnect
       curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
@@ -425,22 +402,14 @@ int main() {
     string setLocStr = "";
     
     // Credentials, Mutual Authentication:
-    unique_ptr<test_credentials> test_creds = unique_ptr<test_credentials>(
-            new test_credentials(
-                mutualAuthFiles.caCrtFile,
-                mutualAuthFiles.clientCrtFile,
-                mutualAuthFiles.clientKeyFile));
+    stringstream ssUri;
+    ssUri << host;
+    auto channel_creds = grpc::SslCredentials(grpc::SslCredentialsOptions());
+    shared_ptr<Channel> channel = grpc::CreateChannel(ssUri.str(), channel_creds);
 
-    grpc::SslCredentialsOptions credentials;
-
-    credentials.pem_root_certs = test_creds->caCrt;
-    credentials.pem_cert_chain = test_creds->clientCrt;
-    credentials.pem_private_key = test_creds->clientKey;
-
-    auto channel_creds = grpc::SslCredentials(grpc::SslCredentialsOptions(credentials));
-    shared_ptr<Channel> channel = grpc::CreateChannel(host, channel_creds);
-
+    cout << "Url to use: " << ssUri.str() << endl;
     unique_ptr<MexGrpcClient> mexClient = unique_ptr<MexGrpcClient>(new MexGrpcClient(channel));
+
 
     try {
         shared_ptr<Loc> loc = mexClient->retrieveLocation();
@@ -474,12 +443,13 @@ int main() {
 	    	     
 	    if (!grpcStatus.ok()) {
 	      cout << "Test Case Passed!! Received Error: " << grpcStatus.error_message() << endl << endl;
-	    } else {
-	      // This test case does not receive a LOC_ERROR_UNAUTHORIZED .. EDGECLOUD-369 is open for this issue
+	    } else
+	      if (verifyLocationReply.gps_location_status() == 6){
 	      // Print some reply values out:
 	      cout << "GRPC VerifyLocation Status: " << grpcStatus.error_code() << endl
 		   << "GPS Location Status: " << verifyLocationReply.gps_location_status() << endl
 		   << "GPS Location Accuracy: " << verifyLocationReply.gps_location_accuracy_km() << "km"<< endl
+	           << "Test Case Passed!! Received Status Unknown" << endl
 		   << endl;
 	    }
 	    exit(0);
