e2d2e0ab670ce92684d438b261d85dbe24959b8f
Andy Anderson <andy.anderson@mobiledgex.com>
2019-08-30 13:44:35 -0500

    add shared_variables_mc.py  Changes to be committed: 	new file:   ../../modules/shared_variables_mc.py

---
+++ modules/shared_variables_mc.py
@@ -0,0 +1,9 @@
+import time
+
+default_time_stamp = str(int(time.time()))
+#default_time_stamp = str(time.time()).replace('.', '-')
+#username_default = 'user' + default_time_stamp
+#password_default = 'password' + default_time_stamp
+username_default = 'mexadmin'
+password_default = 'mexadmin123'
+email_default = username_default + '@email.com'
