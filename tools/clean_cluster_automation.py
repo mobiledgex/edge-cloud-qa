8a19e01d8e98d53da599415db3e27163d3522151
Andy Anderson <andy.anderson@mobiledgex.com>
2019-09-27 11:31:44 -0500

    added clean_cluster_automation.py to clean clusters off of openstack so DeleteCloudlet will work  Changes to be committed: 	modified:   run_edgecloud.jenkinsfile 	new file:   ../tools/clean_cluster_automation.py

---
+++ tools/clean_cluster_automation.py
@@ -0,0 +1,84 @@
+#!/usr/local/bin/python3
+
+# copy needed proto files into 1 directory
+# I originally tried building these from the original path but ran into problems
+
+import os
+import sys
+import argparse
+import logging
+import MexController as mex_controller
+
+parser = argparse.ArgumentParser(description='clean prov from controller')
+parser.add_argument('--address', default='127.0.0.1:55001', help='controller address:port default is 127.0.0.1:55001')
+
+args = parser.parse_args()
+
+controller_address = args.address
+
+mex_root_cert = 'mex-ca.crt'
+mex_cert = 'localserver.crt'
+mex_key = 'localserver.key'
+
+
+appinst_delete = [{'cloudlet_name':'automationBonnCloudlet'}]
+clusterinst_delete = [{'cloudlet_name':'automationBonnCloudlet'}]
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+controller = mex_controller.MexController(controller_address = controller_address,
+                                       root_cert = mex_root_cert,
+                                       key = mex_key,
+                                       client_cert = mex_cert
+)
+
+def clean_appinst():
+    print('clean appinst')
+    appinst_list = controller.show_app_instances()
+    if len(appinst_list) == 0:
+        print('nothing to delete')
+    else:
+        for a in appinst_list:
+            #print('aaa',a.key.app_key.name)
+            if not in_appinst_list(a):
+                print('keeping', a.key.app_key.name)
+            else:
+                print('***deleting', a.key.app_key.name)
+                #a.crm_override = 1
+                controller.delete_app_instance(a)
+
+def clean_clusterinst():
+    print('clean cluster instance')
+    app_list = controller.show_cluster_instances()
+    if len(app_list) == 0:
+        print('nothing to delete')
+    else:
+        for a in app_list:
+            #print('aaa',a.key.cluster_key.name, a.key.cloudlet_key.name, a.key.cloudlet_key.operator_key.name)
+            if not in_clusterinst_list(a):
+                print('keeping', a.key.cluster_key.name)
+            else:
+                print('**** deleting', a.key.cluster_key.name)
+                #a.crm_override = 4
+                try:
+                    controller.delete_cluster_instance(a)
+                except:
+                    print('error deleting.continuing to next item')
+
+def in_appinst_list(app):
+    for a in appinst_delete:
+        #print(a['cloudlet_name'], app.key.cluster_inst_key.cloudlet_key.name)
+        if a['cloudlet_name'] == app.key.cluster_inst_key.cloudlet_key.name:
+            #print('found app in appinst_del_list', app.key.name)
+            return True
+
+def in_clusterinst_list(app):
+    for a in clusterinst_delete:
+        print()
+        if a['cloudlet_name'] == app.key.cloudlet_key.name:
+            print('found clusterinst in clusterinst_keep_list', app.key.cluster_key.name)
+            return True
+
+clean_appinst()
+clean_clusterinst()
