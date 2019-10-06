a318a6e3fdc25aa95534938540c82da19c4ea36d
Andy Anderson <andy.anderson@mobiledgex.com>
2019-10-06 16:13:41 -0500

    update tests for dedicated/shared openstack vars. split jenkins opentstack tests into dedicated/shared

--- testcases/config/automation_variables.py
+++ testcases/config/automation_variables.py
@@ -2,6 +2,8 @@ test_timeout_crm = '32 min'
 cloudlet_name_azure = 'automationAzureCentralCloudlet'
 #cloudlet_name_openstack = 'automationHamburgCloudlet'
 cloudlet_name_openstack = 'automationBonnCloudlet'
+cloudlet_name_openstack_shared = 'automationBonnCloudlet'
+cloudlet_name_openstack_dedicated = 'automationMunichCloudlet'
 
 docker_image = 'docker-qa.mobiledgex.net/mobiledgex/images/server_ping_threaded:5.0'
 
