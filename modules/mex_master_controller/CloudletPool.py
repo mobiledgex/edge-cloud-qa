e469e5c100528662127cecea74073afe84e93bb1
Andy Anderson <andy.anderson@mobiledgex.com>
2020-01-06 15:54:40 -0600

    update MexMasterController for split out of classes  Changes to be committed: 	modified:   ../../../modules/MexMasterController.py 	new file:   ../../../modules/mex_master_controller/AppInstance.py 	modified:   ../../../modules/mex_master_controller/CloudletPool.py 	new file:   ../../../modules/mex_master_controller/Flavor.py 	new file:   ../../../modules/mex_master_controller/Metrics.py 	modified:   ../../../modules/mex_master_controller/MexOperation.py

--- modules/mex_master_controller/CloudletPool.py
+++ modules/mex_master_controller/CloudletPool.py
@@ -39,6 +39,10 @@ class CloudletPool(MexOperation):
         msg = self._build(cloudlet_pool_name=cloudlet_pool_name, use_defaults=use_defaults)
         msg_dict = {'cloudletpool': msg}
 
+        thread_name = None
+        if 'key' in msg:
+            thread_name = msg['key']['name']
+            
         msg_dict_delete = None
         if auto_delete and 'key' in msg:
             msg_delete = self._build(cloudlet_pool_name=msg['key']['name'], use_defaults=False)
@@ -49,7 +53,7 @@ class CloudletPool(MexOperation):
             msg_show = self._build(cloudlet_pool_name=msg['key']['name'], use_defaults=False)
             msg_dict_show = {'cloudletpool': msg_show}
         
-        return self.create(token=token, url=self.create_url, delete_url=self.delete_url, show_url=self.show_url, region=region, json_data=json_data, use_defaults=use_defaults, use_thread=use_thread, create_msg=msg_dict, delete_msg=msg_dict_delete, show_msg=msg_dict_show)
+        return self.create(token=token, url=self.create_url, delete_url=self.delete_url, show_url=self.show_url, region=region, json_data=json_data, use_defaults=use_defaults, use_thread=use_thread, create_msg=msg_dict, delete_msg=msg_dict_delete, show_msg=msg_dict_show, thread_name=thread_name)
 
     def delete_cloudlet_pool(self, token=None, region=None, cloudlet_pool_name=None, json_data=None, use_defaults=True, use_thread=False):
         msg = self._build(cloudlet_pool_name=cloudlet_pool_name, use_defaults=use_defaults)
