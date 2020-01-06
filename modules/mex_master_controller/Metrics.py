5a92e09a4bfd69ebf9c4bc573ebca3cee702664d
Andy Anderson <andy.anderson@mobiledgex.com>
2020-01-06 15:54:40 -0600

    update MexMasterController for split out of classes  Changes to be committed: 	modified:   ../../../modules/MexMasterController.py 	new file:   ../../../modules/mex_master_controller/AppInstance.py 	modified:   ../../../modules/mex_master_controller/CloudletPool.py 	new file:   ../../../modules/mex_master_controller/Flavor.py 	new file:   ../../../modules/mex_master_controller/Metrics.py 	modified:   ../../../modules/mex_master_controller/MexOperation.py

---
+++ modules/mex_master_controller/Metrics.py
@@ -0,0 +1,44 @@
+import json
+import logging
+
+import shared_variables
+
+from mex_master_controller.MexOperation import MexOperation
+from mex_master_controller.AppInstance import AppInstance
+
+logger = logging.getLogger('mex_mastercontroller rest')
+
+
+class Metrics(MexOperation):
+    def __init__(self, root_url, prov_stack=None, token=None, super_token=None):
+        super().__init__(root_url=root_url, prov_stack=prov_stack, token=token, super_token=super_token)
+
+        self.client_url = '/auth/metrics/client'
+
+    # '{"region":"local","appinst":{"app_key":{"developer_key":{"name":"AcmeAppCo"},"name":"someapplication1","version":"1.0"}},"method":"FindCloudlet","cellid":1234,"selector":"api","last":2}
+    def _build(self, type_dict=None, method=None, cellid=None, selector=None, last=None, start_time=None, end_time=None, use_defaults=True):
+        metric_dict = {}
+
+        if type_dict is not None:
+            metric_dict.update(type_dict)
+        if selector is not None:
+            metric_dict['selector'] = selector
+        if last is not None:
+            try:
+                metric_dict['last'] = int(last)
+            except:
+                metric_dict['last'] = last
+        if start_time is not None:
+            metric_dict['starttime'] = start_time
+        if end_time is not None:
+            metric_dict['endtime'] = end_time
+
+        return metric_dict
+
+    def get_find_cloudlet_api_metrics(self, token=None, region=None, app_name=None, developer_name=None, app_version=None, selector=None, last=None, start_time=None, end_time=None, cellid=None, json_data=None, use_defaults=True, use_thread=False):
+        app_inst = AppInstance(app_name=app_name, developer_name=developer_name, app_version=app_version, use_defaults=False)
+        
+        print('*WARN*', app_inst)
+        msg_dict = self._build(type_dict=app_inst, method='FindCloudlet', cellid=cellid, selector='api', last=last, start_time=start_time, end_time=end_time)
+
+        return self.show(token=token, url=self.client_url, region=region, json_data=json_data, use_defaults=use_defaults, use_thread=use_thread, message=msg_dict)
