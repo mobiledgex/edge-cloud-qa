191212bbe42c71bd76ba16534e1232c0e775d982
Leon Adams <leon.adams@mobiledgex.com>
2020-01-13 07:35:42 -0600

    Initial commit 	new file:   cloudletverification_vars.py

---
+++ testcases/config/cloudletverification_vars.py
@@ -0,0 +1,31 @@
+test_timeout_crm = '60 min'
+
+
+cloudlet_name_openstack_create = 'verificationCloudlet'
+
+verification_region = 'EU'
+
+physical_name_openstack = 'munich'
+operator_name_openstack = 'TDG'
+
+cloudlet_name_openstack = 'automationMunichCloudlet'
+cloudlet_name_openstack_metrics = 'automationMunichCloudlet'
+
+cloudlet_latitude = '53.5461'
+cloudlet_longitude = '-113.4938'
+
+docker_image = 'docker-qa.mobiledgex.net/mobiledgex/images/server_ping_threaded:5.0'
+
+artifactory_dummy_image_name = 'execJira.py'
+
+qcow_centos_image = 'https://artifactory-qa.mobiledgex.net/artifactory/mobiledgex/server_ping_threaded_centos7.qcow2#md5:5ce8dbcdd8b7c2054779d742f4bf602d'
+qcow_centos_image_notrunning = 'https://artifactory-qa.mobiledgex.net/artifactory/mobiledgex/server_ping_threaded_notrunning_centos7.qcow2#md5:7a08091f71f1e447ce291e467cc3926c'
+qcow_windows_image = 'https://artifactory-qa.mobiledgex.net/artifactory/mobiledgex/server_ping_threaded_windows2012.qcow2#md5:42171406daca80298098ac314200634a'
+qcow_centos_openstack_image = 'server_ping_threaded_centos7'
+
+#vm_console_address =  'https://hamedgecloud.telekom.de:6080/vnc_auto.html'
+vm_console_address =  'https://bonnedgecloud.telekom.de:6080/vnc_auto.html'
+
+mextester99_gmail_password = 'rfbixqomqidobmcb'
+
+token_server_url = 'http://mexdemo.tok.mobiledgex.net:9999/its?followURL=https://dme.mobiledgex.net/verifyLoc'
