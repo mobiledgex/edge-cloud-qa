329680f1cda3ce81a5c846d67213b6c1dd75c8ca
Leon Adams <leon.adams@mobiledgex.com>
2020-01-16 19:34:13 -0600

    Reduced the nodes to 2 and updagted the vars file 	modified:   cloudletverification_createCluster_openstack_Dedicated_k8s.robot 	modified:   cloudletverification_createCluster_openstack_Dedicated_numNodes.robot 	modified:   cloudletverification_createCluster_openstack_Shared_k8s.robot 	modified:   cloudletverification_createCluster_openstack_numNodes.robot 	modified:   ../config/cloudletverification_vars.py

--- testcases/config/cloudletverification_vars.py
+++ testcases/config/cloudletverification_vars.py
@@ -14,11 +14,11 @@ cloudlet_name_openstack_metrics = 'montreal-pitfield'
 cloudlet_latitude = '45.5017'
 cloudlet_longitude = '-73.5673'
 
-docker_image = 'docker-qa.mobiledgex.net/mobiledgex/images/server_ping_threaded:5.0'
+docker_image = 'docker.mobiledgex.net/mobiledgex/images/server_ping_threaded:5.0'
 
-qcow_centos_image = 'https://artifactory-qa.mobiledgex.net/artifactory/mobiledgex/server_ping_threaded_centos7.qcow2#md5:5ce8dbcdd8b7c2054779d742f4bf602d'
-qcow_centos_image_notrunning = 'https://artifactory-qa.mobiledgex.net/artifactory/mobiledgex/server_ping_threaded_notrunning_centos7.qcow2#md5:7a08091f71f1e447ce291e467cc3926c'
-qcow_windows_image = 'https://artifactory-qa.mobiledgex.net/artifactory/mobiledgex/server_ping_threaded_windows2012.qcow2#md5:42171406daca80298098ac314200634a'
+qcow_centos_image = 'https://artifactory.mobiledgex.net/artifactory/mobiledgex/server_ping_threaded_centos7.qcow2#md5:5ce8dbcdd8b7c2054779d742f4bf602d'
+qcow_centos_image_notrunning = 'https://artifactory.mobiledgex.net/artifactory/mobiledgex/server_ping_threaded_notrunning_centos7.qcow2#md5:7a08091f71f1e447ce291e467cc3926c'
+qcow_windows_image = 'https://artifactory.mobiledgex.net/artifactory/mobiledgex/server_ping_threaded_windows2012.qcow2#md5:42171406daca80298098ac314200634a'
 qcow_centos_openstack_image = 'server_ping_threaded_centos7'
 
 
