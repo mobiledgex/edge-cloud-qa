46b23904b3b460c8ba74afa83733fd1261693903
Andy Anderson <andy.anderson@mobiledgex.com>
2020-01-20 12:16:48 -0600

    update vars file for session cookie and dusseldorf  Changes to be committed: 	modified:   ../config/automation_variables.py

--- testcases/config/automation_variables.py
+++ testcases/config/automation_variables.py
@@ -8,15 +8,17 @@ cloudlet_name_openstack_shared = 'automationBonnCloudlet'
 cloudlet_name_openstack_dedicated = 'automationMunichCloudlet'
 cloudlet_name_openstack_vm = 'automationMunichCloudlet'
 cloudlet_name_openstack_metrics = 'automationBerlinCloudlet'
+cloudlet_name_openstack_gpu = 'automationDusseldorfCloudlet'
 
 operator_name_azure = 'azure'
 operator_name_gcp = 'gcp'
 
 docker_image = 'docker-qa.mobiledgex.net/mobiledgex/images/server_ping_threaded:5.0'
+docker_image_developer = 'mobiledgex'
 
 artifactory_dummy_image_name = 'execJira.py'
 
-qcow_centos_image = 'https://artifactory-qa.mobiledgex.net/artifactory/mobiledgex/server_ping_threaded_centos7.qcow2#md5:5ce8dbcdd8b7c2054779d742f4bf602d'
+qcow_centos_image = 'https://artifactory-qa.mobiledgex.net/artifactory/repo-mobiledgex/server_ping_threaded_centos7.qcow2#md5:5ce8dbcdd8b7c2054779d742f4bf602d'
 qcow_centos_image_notrunning = 'https://artifactory-qa.mobiledgex.net/artifactory/mobiledgex/server_ping_threaded_notrunning_centos7.qcow2#md5:7a08091f71f1e447ce291e467cc3926c'
 qcow_windows_image = 'https://artifactory-qa.mobiledgex.net/artifactory/mobiledgex/server_ping_threaded_windows2012.qcow2#md5:42171406daca80298098ac314200634a'
 qcow_centos_openstack_image = 'server_ping_threaded_centos7'
@@ -27,3 +29,4 @@ vm_console_address =  'https://bonnedgecloud.telekom.de:6080/vnc_auto.html'
 mextester99_gmail_password = 'rfbixqomqidobmcb'
 
 token_server_url = 'http://mexdemo.tok.mobiledgex.net:9999/its?followURL=https://dme.mobiledgex.net/verifyLoc'
+expired_cookie = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1Nzk0MjE2MTMsImlhdCI6MTU3OTMzNTIxMywia2V5Ijp7InBlZXJpcCI6IjEwLjI0MC4wLjQiLCJkZXZuYW1lIjoiZGV2ZWxvcGVyMTU3OTMzNTIxMC0zMDA1NzU1IiwiYXBwbmFtZSI6ImFwcDE1NzkzMzUyMTAtMzAwNTc1NSIsImFwcHZlcnMiOiIxLjAiLCJ1bmlxdWVpZHR5cGUiOiJkbWUta3N1aWQiLCJ1bmlxdWVpZCI6IjFXWW41YjNBOTFYVmFuWERQN1pNdEFUcEIySCIsImtpZCI6Mn19.lAlRGDeRCQ-iCXCakblPeuKiflQ2WssbmNEsIm08mutoFzsqLNPS484eZaUPVzSxn5U92zfbjpWnIaLP3GxBYg'
