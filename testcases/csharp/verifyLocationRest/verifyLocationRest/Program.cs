957ccedd1b764205b6a086d7aa58972422e8a73c
Leon Adams <leon.adams@mobiledgex.com>
2020-01-27 16:33:15 -0600

    modified:   VerifyLocationMisMatchSameCountry/VerifyLocationMisMatchSameCountry/Program.cs 	modified:   VerifyLocationMisMatchSameCountry/VerifyLocationMisMatchSameCountry/VerifyLocationMisMatchSameCountry.csproj 	modified:   verifyGPSLocation/verifyGPSLocation/Program.cs 	modified:   verifyGPSLocation/verifyGPSLocation/verifyGPSLocation.csproj 	modified:   verifyLocation100KM/verifyLocation100KM/Program.cs 	modified:   verifyLocation100KM/verifyLocation100KM/verifyLocation100KM.csproj 	modified:   verifyLocation100KMRest/verifyLocation100KMRest/Program.cs 	modified:   verifyLocation100KMRest/verifyLocation100KMRest/verifyLocation100KMRest.csproj 	modified:   verifyLocation10KM/verifyLocation10KM/Program.cs 	modified:   verifyLocation10KM/verifyLocation10KM/verifyLocation10KM.csproj 	modified:   verifyLocation10KMRest/verifyLocation10KMRest/Program.cs 	modified:   verifyLocation10KMRest/verifyLocation10KMRest/verifyLocation10KMRest.csproj 	modified:   verifyLocationBadCarrier/verifyLocationBadCarrier/Program.cs 	modified:   verifyLocationBadCarrier/verifyLocationBadCarrier/verifyLocationBadCarrier.csproj 	modified:   verifyLocationBadCarrierRest/verifyLocationBadCarrierRest/Program.cs 	modified:   verifyLocationBadCarrierRest/verifyLocationBadCarrierRest/verifyLocationBadCarrierRest.csproj 	modified:   verifyLocationBadCookie/verifyLocationBadCookie/Program.cs 	modified:   verifyLocationBadCookie/verifyLocationBadCookie/verifyLocationBadCookie.csproj 	modified:   verifyLocationBadCookieRest/verifyLocationBadCookieRest/Program.cs 	modified:   verifyLocationBadCookieRest/verifyLocationBadCookieRest/verifyLocationBadCookieRest.csproj 	modified:   verifyLocationMisMatchOtherCountry/verifyLocationMisMatchOtherCountry/Program.cs 	modified:   verifyLocationMisMatchOtherCountry/verifyLocationMisMatchOtherCountry/verifyLocationMisMatchOtherCountry.csproj 	modified:   verifyLocationMisMatchOtherCountryRest/verifyLocationMisMatchOtherCountryRest/Program.cs 	modified:   verifyLocationMisMatchOtherCountryRest/verifyLocationMisMatchOtherCountryRest/verifyLocationMisMatchOtherCountryRest.csproj 	modified:   verifyLocationMisMatchSameCountryRest/verifyLocationMisMatchSameCountryRest/Program.cs 	modified:   verifyLocationMisMatchSameCountryRest/verifyLocationMisMatchSameCountryRest/verifyLocationMisMatchSameCountryRest.csproj 	modified:   verifyLocationRest/verifyLocationRest/Program.cs 	modified:   verifyLocationRest/verifyLocationRest/verifyLocationRest.csproj 	modified:   verifyLocationRoamingCountryMatch/verifyLocationRoamingCountryMatch/Program.cs 	modified:   verifyLocationRoamingCountryMatch/verifyLocationRoamingCountryMatch/verifyLocationRoamingCountryMatch.csproj 	modified:   verifyLocationRoamingCountryMatchRest/verifyLocationRoamingCountryMatchRest/Program.cs 	modified:   verifyLocationRoamingCountryMatchRest/verifyLocationRoamingCountryMatchRest/verifyLocationRoamingCountryMatchRest.csproj 	modified:   verifyLocationRoamingCountryMisMatch/verifyLocationRoamingCountryMisMatch/Program.cs 	modified:   verifyLocationRoamingCountryMisMatch/verifyLocationRoamingCountryMisMatch/verifyLocationRoamingCountryMisMatch.csproj 	modified:   verifyLocationRoamingCountryMisMatchRest/verifyLocationRoamingCountryMisMatchRest/Program.cs 	modified:   verifyLocationRoamingCountryMisMatchRest/verifyLocationRoamingCountryMisMatchRest/verifyLocationRoamingCountryMisMatchRest.csproj

--- testcases/csharp/verifyLocationRest/verifyLocationRest/Program.cs
+++ testcases/csharp/verifyLocationRest/verifyLocationRest/Program.cs
@@ -12,17 +12,17 @@ namespace RestSample
 {
     class Program
     {
-        static string tokenServerURI = "http://mextest.tok.mobiledgex.net:9999/its?followURL=https://dme.mobiledgex.net/verifyLoc";
+        static string tokenServerURI = "http://mexdemo.tok.mobiledgex.net:9999/its?followURL=https://dme.mobiledgex.net/verifyLoc";
         static string carrierName = "tmus";
         //static string appName = "EmptyMatchEngineApp";
         //static string devName = "EmptyMatchEngineApp";
-        static string devName = "automation_api";
+        static string devName = "mobiledgex";
         static string appName = "automation_api_app";
         static string appVers = "1.0";
         static string developerAuthToken = "";
 
         //static string host = "tdg.dme.mobiledgex.net";
-        static string host = "automationbonn.dme.mobiledgex.net";
+        static string host = "us-qa.dme.mobiledgex.net";
         static UInt32 port = 38001;
         static string sessionCookie;
 
@@ -52,7 +52,7 @@ namespace RestSample
             //Console.WriteLine(clientIP);
 
             string resp = null;
-            string serverURL = "http://mextest.locsim.mobiledgex.net:8888/updateLocation";
+            string serverURL = "http://mexdemo.locsim.mobiledgex.net:8888/updateLocation";
             string payload = "{" + '"' + "latitude" + '"' + ':' + locLat + ',' + ' ' + '"' + "longitude" + '"' + ':' + locLong + ',' + ' ' + '"' + "ipaddr" + '"' + ':' + '"' + clientIP + '"' + "}";
             Console.WriteLine(payload);
             byte[] postBytes = Encoding.UTF8.GetBytes(payload);
@@ -126,7 +126,7 @@ namespace RestSample
                 long timeLongMs = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
                 long seconds = timeLongMs / 1000;
                 int nanoSec = (int)(timeLongMs % 1000) * 1000000;
-                var ts = new Timestamp { nanos = nanoSec, seconds = seconds };
+                var ts = new Timestamp { nanos = nanoSec, seconds = seconds.ToString() };
                 var loc = new Loc()
                 {
                     course = 0,
