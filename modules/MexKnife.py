0925c8da2dfcd7218bd0b3c6291de0d1e7f27d4a
Angshuman Bhattacharjee <angshuman.bhattacharjee@mobiledgex.com>
2020-06-17 22:42:03 +0530

    Upgrade Cloudlet using chef

---
+++ modules/MexKnife.py
@@ -0,0 +1,88 @@
+import logging
+import subprocess
+import time
+import sys
+import os
+
+
+class MexKnife:
+    def __init__(self, policy_group='qa'):
+        self.policy_group = policy_group
+        
+
+    def upgrade_cloudlet(self, cloudlet_names=None, container_version=None):
+        for i in cloudlet_names:
+            logging.info('cloudlet name is ' + i)
+        self.before_upgrade(cloudlet_names)
+        self.start_upgrade()
+        self.verify_upgrade(cloudlet_names,container_version)
+
+
+    def before_upgrade(self, cloudlets):
+        cmd_knife = 'knife exec -E \'nodes.find("name:qa*pf") {|n| puts n.name+"="+n["edgeCloudVersion"]}\''
+        output = self.run_cmd(cmd_knife)
+        crm_list = output.splitlines()
+        for line in crm_list:
+            logging.info(str(line))
+
+ 
+    def start_upgrade(self):
+        policy_group = self.policy_group
+        workdir = os.environ.get("WORKSPACE")
+        if not workdir:
+            workdir = os.environ.get("HOME")
+        chef_dir = os.path.join(workdir, 'go/src/github.com/mobiledgex/edge-cloud-infra/chef/policyfiles')
+        os.chdir(chef_dir)
+        retval = os.getcwd() 
+        logging.info('Current directory is ' + str(retval))
+        cmdchef = f'chef push {policy_group} docker_crm.rb'
+        output = self.run_cmd(cmdchef)
+        list1 = output.splitlines()
+        for line in list1:
+            if 'Failed to upload policy' in line:
+                logging.error(line)
+                raise Exception('ERROR: line')  
+
+
+    def run_cmd(self, command):
+        logging.info(f'running cmd: {command}')
+        cmd_return = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        cmd_out = cmd_return.stdout.decode('utf-8')
+        cmd_err = cmd_return.stderr.decode('utf-8')
+        if cmd_err:
+            raise Exception(cmd_err)
+        return cmd_out
+
+
+    def verify_upgrade(self, cloudlets, version):
+        logging.info('version to be upgraded to is ' + str(version))
+        cmd_knife = 'knife exec -E \'nodes.find("name:qa*pf") {|n| puts n.name+"="+n["edgeCloudVersion"]}\''
+        output = self.run_cmd(cmd_knife)
+        crm_list = output.splitlines() 
+        logging.debug(output)
+
+        cloudlet_list = []
+        x = 0
+        while((x < 30) and (len(cloudlet_list) < 4)):            
+            for i in cloudlets:
+                for line in crm_list:
+                    if i in line and version in line:
+                        logging.info(i + ' has been upgraded to ' + version)
+                        cloudlet_list.append(i)
+            time.sleep(30)
+            x+=1
+
+        length = len(cloudlet_list)            
+        logging.info('Length of cloudlet_list is ' + str(length))
+
+        if not cloudlet_list:
+            raise Exception('ERROR: None of the cloudlets got upgraded to ' + version)
+        
+        elif(len(cloudlet_list) < 4):            
+             raise Exception('ERROR: Not all the cloudlets not upgraded to ' + version) 
+
+        else:
+             logging.info('All the cloudlets got upgraded to ' + version)
+
+
+    
