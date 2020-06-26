76844fc3cb92085dc9c9a87261389dc99344a048
Angshuman Bhattacharjee <angshuman.bhattacharjee@mobiledgex.com>
2020-06-26 22:55:28 +0530

    Skip Verify Email , docker compose and show logs script changes

--- modules/MexMasterController.py
+++ modules/MexMasterController.py
@@ -2199,6 +2199,8 @@ class MexMasterController(MexRest):
         return self.rundebug.run_debug(timeout=timeout, node_name=node_name, node_type=node_type, region=region, cloudlet_name=cloudlet_name, operator_org_name=operator_org_name, args=args, command=command, pretty=pretty, token=token, json_data=json_data, use_defaults=use_defaults, use_thread=use_thread)
 
     def skip_verify_email(self, skip_verify_email=True, token=None, use_defaults=True, use_thread=False):
+        if token is None:
+            token=self.super_token
         return self.config.skip_verify_config(token=token, skip_verify_email=skip_verify_email, use_defaults=use_defaults, use_thread=use_thread)
 
     def cleanup_provisioning(self):
