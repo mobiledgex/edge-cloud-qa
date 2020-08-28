124b0b4f1686adad39abbe56db1069ee3614ade9
ashutoshbhatt1 <ashutosh567@gmail.com>
2020-08-28 16:57:41 -0500

    updated modules for HA support

--- modules/mex_master_controller/App.py
+++ modules/mex_master_controller/App.py
@@ -24,6 +24,7 @@ class App(MexOperation):
         _app_version_field_number = "2.3"
         _developer_name_field_number = "2.1.2"
         _accessports_field_number = "7"
+        _autoprov_field_number = "28"
 
         if app_name == 'default': app_name = shared_variables.app_name_default
         if app_version == 'default': app_version = shared_variables.app_version_default
@@ -130,8 +131,9 @@ class App(MexOperation):
             app_dict['official_fqdn'] = official_fqdn
         if annotations:
             app_dict['annotations'] = annotations
-        if auto_prov_policy:
+        if auto_prov_policy is not None:
             app_dict['auto_prov_policy'] = auto_prov_policy
+            _fields_list.append(_autoprov_field_number)
         if configs_kind:
             configs_dict['kind'] = configs_kind
         if configs_config:
