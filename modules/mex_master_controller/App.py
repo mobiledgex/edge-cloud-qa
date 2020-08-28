c099b6a0749c203fcf4b423e8290c92e57cca8e8
Andy Anderson <andy.anderson@mobiledgex.com>
2020-08-28 14:20:06 -0500

    update update_app_instance, add refresh_app_inst 	modified:   MexMasterController.py 	modified:   mex_master_controller/App.py 	modified:   mex_master_controller/AppInstance.py 	modified:   mex_master_controller/MexOperation.py

--- modules/mex_master_controller/App.py
+++ modules/mex_master_controller/App.py
@@ -24,6 +24,7 @@ class App(MexOperation):
         _app_version_field_number = "2.3"
         _developer_name_field_number = "2.1.2"
         _accessports_field_number = "7"
+        _official_fqdn_field_number = '25'
 
         if app_name == 'default': app_name = shared_variables.app_name_default
         if app_version == 'default': app_version = shared_variables.app_version_default
@@ -128,6 +129,7 @@ class App(MexOperation):
             app_dict['scale_with_cluster'] = True
         if official_fqdn:
             app_dict['official_fqdn'] = official_fqdn
+            _fields_list.append(_official_fqdn_field_number)
         if annotations:
             app_dict['annotations'] = annotations
         if auto_prov_policy:
