3e30f07487b2b3f4560ad0c2c3abd8301c63d1a0
Andy Anderson <andy.anderson@mobiledgex.com>
2020-09-02 13:13:46 -0500

    add test for cloudlet state 	modified:   ../../../modules/MexMasterController.py 	modified:   ../../../modules/mex_master_controller/Cloudlet.py 	modified:   ../../../modules/mex_master_controller/MexOperation.py 	modified:   ../../crm/cloudlet/cloudlet_accessvars.robot 	new file:   findCloudlet_cloudlet_offline.robot

--- modules/mex_master_controller/Cloudlet.py
+++ modules/mex_master_controller/Cloudlet.py
@@ -16,6 +16,8 @@ class Cloudlet(MexOperation):
         self.create_url = '/auth/ctrl/CreateCloudlet'
         self.delete_url = '/auth/ctrl/DeleteCloudlet'
         self.show_url = '/auth/ctrl/ShowCloudlet'
+        self.show_info_url = '/auth/ctrl/ShowCloudletInfo'
+        self.inject_info_url = '/auth/ctrl/InjectCloudletInfo'
         self.update_url = '/auth/ctrl/UpdateCloudlet'
         self.metrics_url = '/auth/metrics/cloudlet'
         self.addmapping_url = '/auth/ctrl/AddCloudletResMapping'
@@ -150,7 +152,6 @@ class Cloudlet(MexOperation):
                 elif 'OPENRC_DATA' in var:
                     key,value = var.split('OPENRC_DATA=')
                     key = 'OPENRC_DATA'
-                print('*WARN*', 'kv',key,value)
                 access_dict[key] = value
             cloudlet_dict['access_vars'] = access_dict
             
@@ -201,7 +202,47 @@ class Cloudlet(MexOperation):
 
         return map_dict
 
-    
+    def _build_info(self, cloudlet_dict=None, container_version=None, controller=None, notify_id=None, os_max_ram=None, os_max_vcores=None, os_max_vol_gb=None, state=None, status=None, flavor_name=None, flavor_disk=None, flavor_ram=None, flavor_vcpus=None, use_defaults=True):
+        info_dict = {}
+        if cloudlet_dict is not None:
+            info_dict.update(cloudlet_dict)
+
+        if container_version is not None:
+            info_dict['containerversion'] = container_version
+        if controller is not None:
+            info_dict['controller'] = controller
+        if notify_id is not None:
+            info_dict['notifyid'] = notify_id
+        if os_max_ram is not None:
+            info_dict['osmaxram'] = osmaxram
+        if os_max_vcores is not None:
+            info_dict['osmaxvcores'] = osmaxvcores
+        if os_max_vol_gb is not None:
+            info_dict['osmaxvolgb'] = osmaxvolgb
+        if state is not None:
+            if state == 'CloudletStateUnknown':
+                info_dict['cloudletinfo']['state'] = 0
+            elif state == 'CloudletStateErrors':
+                info_dict['cloudletinfo']['state'] = 1
+            elif state == 'CloudletStateReady':
+                info_dict['cloudletinfo']['state'] = 2
+            elif state == 'CloudletStateOffline':
+                info_dict['cloudletinfo']['state'] = 3
+            elif state == 'CloudletStateNotPresent':
+                info_dict['cloudletinfo']['state'] = 4
+            elif state == 'CloudletStateInit':
+                info_dict['cloudletinfo']['state'] = 5
+            elif state == 'CloudletStateUpgrade':
+                info_dict['cloudletinfo']['state'] = 6
+            elif state == 'CloudletStateNeedSync':
+                info_dict['cloudletinfo']['state'] = 7
+
+        if status is not None:
+            info_dict['status'] = status
+
+
+        return info_dict
+ 
     def create_cloudlet(self, token=None, region=None, operator_org_name=None, cloudlet_name=None, latitude=None, longitude=None, number_dynamic_ips=None, ip_support=None, platform_type=None, physical_name=None, env_vars=None, access_vars=None, vm_pool=None, crm_override=None, notify_server_address=None, deployment_local=None, container_version=None, override_policy_container_version=None, json_data=None, use_defaults=True, use_thread=False, auto_delete=True):
         msg = self._build(cloudlet_name=cloudlet_name, operator_org_name=operator_org_name, number_dynamic_ips=number_dynamic_ips, latitude=latitude, longitude=longitude, ip_support=ip_support, platform_type=platform_type, physical_name=physical_name, env_vars=env_vars, access_vars=access_vars, vm_pool=vm_pool, deployment_local=deployment_local, container_version=container_version, override_policy_container_version=override_policy_container_version, crm_override=crm_override, notify_server_address=notify_server_address, use_defaults=use_defaults)
         msg_dict = {'cloudlet': msg}
@@ -231,6 +272,24 @@ class Cloudlet(MexOperation):
 
         return self.show(token=token, url=self.show_url, region=region, json_data=json_data, use_defaults=use_defaults, use_thread=use_thread, message=msg_dict)
 
+    def show_cloudlet_info(self, token=None, region=None, operator_org_name=None, cloudlet_name=None, json_data=None, use_defaults=False, use_thread=False):
+        msg = self._build(cloudlet_name=cloudlet_name, operator_org_name=operator_org_name, use_defaults=use_defaults)
+        msg_dict = {'cloudletinfo': msg}
+
+        return self.show(token=token, url=self.show_info_url, region=region, json_data=json_data, use_defaults=True, use_thread=use_thread, message=msg_dict)
+
+    def inject_cloudlet_info(self, token=None, region=None, operator_org_name=None, cloudlet_name=None, container_version=None, controller=None, notify_id=None, os_max_ram=None, os_max_vcores=None, os_max_vol_gb=None, state=None, status=None, flavor_name=None, flavor_disk=None, flavor_ram=None, flavor_vcpus=None, json_data=None, use_defaults=False, use_thread=False):
+        msg = self._build(cloudlet_name=cloudlet_name, operator_org_name=operator_org_name, use_defaults=False)
+        inject_dict = {'cloudletinfo': msg}
+        msg_dict = self._build_info(cloudlet_dict=inject_dict, controller=controller, container_version=container_version, notify_id=notify_id, os_max_ram=os_max_ram, os_max_vcores=os_max_vcores, os_max_vol_gb=os_max_vol_gb, state=state, status=status, flavor_name=flavor_name, flavor_disk=flavor_disk, flavor_ram=flavor_ram, flavor_vcpus=flavor_vcpus, use_defaults=False)
+
+        msg_dict_show = None
+        if 'key' in msg and 'name' in msg['key']:
+            msg_show = self._build(cloudlet_name=msg['key']['name'], operator_org_name=msg['key']['organization'], use_defaults=False)
+            msg_dict_show = {'cloudletinfo': msg_show}
+
+        return self.update(token=token, url=self.inject_info_url, show_url=self.show_info_url, region=region, json_data=json_data, use_defaults=True, use_thread=use_thread, message=msg_dict, show_msg=msg_dict_show)
+
     def update_cloudlet(self, token=None, region=None, operator_org_name=None, cloudlet_name=None, latitude=None, longitude=None, number_dynamic_ips=None, ip_support=None, platform_type=None, physical_name=None, env_vars=None, crm_override=None, notify_server_address=None, container_version=None, package_version=None, maintenance_state=None, static_ips=None, json_data=None, use_defaults=True, auto_delete=True, include_fields=True, use_thread=False):
         msg = self._build(cloudlet_name=cloudlet_name, operator_org_name=operator_org_name, number_dynamic_ips=number_dynamic_ips, latitude=latitude, longitude=longitude, ip_support=ip_support, platform_type=platform_type, physical_name=physical_name, container_version=container_version, package_version=package_version, maintenance_state=maintenance_state, static_ips=static_ips, env_vars=env_vars, crm_override=crm_override, notify_server_address=notify_server_address, use_defaults=use_defaults, include_fields=include_fields)
         msg_dict = {'cloudlet': msg}
@@ -244,7 +303,6 @@ class Cloudlet(MexOperation):
 
     def get_cloudlet_metrics(self, token=None, region=None, operator_org_name=None, cloudlet_name=None, selector=None, last=None, start_time=None, end_time=None, json_data=None, use_defaults=True, use_thread=False):
         msg = self._build(cloudlet_name=cloudlet_name, operator_org_name=operator_org_name, use_defaults=False)
-        print('*WARN*', 'msg',msg)
         metric_dict = msg
         if 'key' in msg:
             metric_dict['cloudlet'] = msg['key']
