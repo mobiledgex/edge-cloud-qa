d9162702d9bfb46abce19d67822553f229871a01
Andy Anderson <andy.anderson@mobiledgex.com>
2020-09-19 19:12:15 -0500

    updates for cert changes 	modified:   MexController.py 	new file:   mex_certs.py 	modified:   mex_grpc.py

--- modules/mex_grpc.py
+++ modules/mex_grpc.py
@@ -4,7 +4,7 @@ import os
 import grpc
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(funcName)s line:%(lineno)d - %(message)s',datefmt='%d-%b-%y %H:%M:%S')
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 
 #export GRPC_VERBOSITY=DEBUG
 #export GRPC_TRACE=client_channel,channel,health_check_client,http,http2_stream_state
@@ -19,6 +19,7 @@ class MexGrpc(object):
             root_cert_real = self._findFile(root_cert)
             key_real = self._findFile(key)
             client_cert_real = self._findFile(client_cert)
+
             with open(root_cert_real, 'rb') as f:
                 logger.debug('using root_cert=' + root_cert_real)
                 #trusted_certs = f.read().encode()
@@ -28,16 +29,16 @@ class MexGrpc(object):
                 trusted_key = f.read()
             with open(client_cert_real, 'rb') as f:
                 logger.debug('using client cert=' + client_cert_real)
-                cert = f.read()
+                cert_chain = f.read()
+
             # create credentials
-            credentials = grpc.ssl_channel_credentials(root_certificates=trusted_certs, private_key=trusted_key, certificate_chain=cert)
+            credentials = grpc.ssl_channel_credentials(root_certificates=trusted_certs, private_key=trusted_key, certificate_chain=cert_chain)
             channel_options = [('grpc.keepalive_time_ms',600000),  # 3mins. seems I cannot do less than 5mins. does 5mins anyway if set lower
                                ('grpc.keepalive_timeout_ms', 5000),
                                ('grpc.http2.min_time_between_pings_ms', 60000),
                                ('grpc.http2.max_pings_without_data', 0),
                                ('grpc.keepalive_permit_without_calls', 1)]
             self.grpc_channel = grpc.secure_channel(address, credentials, options=channel_options)
-            print('grpc channel', self.grpc_channel)
         else:
             #self.grpc_channel = grpc.insecure_channel(address)
             credentials = grpc.ssl_channel_credentials()
@@ -47,8 +48,6 @@ class MexGrpc(object):
                                ('grpc.http2.max_pings_without_data', 0),
                                ('grpc.keepalive_permit_without_calls', 1)]
             self.grpc_channel = grpc.secure_channel(address, credentials, options=channel_options)
-            
-        logger.info('xxxxxx1' + str(self.grpc_channel))
 
     def _findFile(self, path):
         for dirname in sys.path:
