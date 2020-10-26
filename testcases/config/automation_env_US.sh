520bd305dc4f6d45bffb737fc7a4dc1038c66360
Andy Anderson <andy.anderson@mobiledgex.com>
2020-10-26 16:10:51 -0500

    add crm pools for regression 	modified:   ../../jenkins/execJira_parallel.py 	new file:   automation_env_EU.sh 	new file:   automation_env_US.sh 	renamed:    openrc_beacon.mex -> openrc_automationBeaconCloudlet.mex 	renamed:    openrc_buckhorn.mex -> openrc_automationBuckhornCloudlet.mex 	renamed:    openrc_paradise.mex -> openrc_automationParadiseCloudlet.mex 	renamed:    openrc_fairview.mex -> openrc_automationFairviewCloudlet.mex 	renamed:    openrc_hawkins.mex -> openrc_automationHawkinsCloudlet.mex 	renamed:    openrc_sunnydale.mex -> openrc_automationSunnydaleCloudlet.mex 	renamed:    openrc_packet_qa.mex -> openrc_packet-qaregression.mex

---
+++ testcases/config/automation_env_US.sh
@@ -0,0 +1,8 @@
+AUTOMATION_CONTROLLER_ADDRESS=mexplat-qa-us.ctrl.mobiledgex.net:55001
+AUTOMATION_DME_ADDRESS=us-qa.dme.mobiledgex.net:50051
+AUTOMATION_DME_REST_ADDRESS=us-qa.dme.mobiledgex.net:38001
+AUTOMATION_DME_CERT=
+AUTOMATION_MC_ADDRESS=console-qa.mobiledgex.net:443
+AUTOMATION_INFLUXDB_ADDRESS=us-qa.influxdb.mobiledgex.net:8086
+AUTOMATION_MC_CERT=
+
