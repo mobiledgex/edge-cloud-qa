2d87bf9127d5e3e97d5cfc6c903041013d6ed6f6
Andy Anderson <andy.anderson@mobiledgex.com>
2020-12-03 18:04:00 -0600

    add test for getcloudletmanifest and revoke key 	modified:   ../../../modules/MexMasterController.py 	modified:   ../../../modules/mex_master_controller/Cloudlet.py 	modified:   ../../../modules/mex_master_controller/MexOperation.py 	new file:   cloudletmanifest_vars.py 	new file:   getCloudletManifest.robot

---
+++ testcases/crm/cloudlet/cloudletmanifest_vars.py
@@ -0,0 +1,23 @@
+manifest_1 =  """manifestitems:
+- id: 1
+  title: Download the MobiledgeX bootstrap VM image (please use your console credentials)
+    from the link
+  contenttype: url
+  contentsubtype: none
+  content: https://artifactory.mobiledgex.net/artifactory/baseimages/mobiledgex-v4.1.2.qcow2"""
+
+manifest_2 = """- id: 2
+  title: Execute the following command to upload the image to your glance store
+  contenttype: command
+  contentsubtype: none
+  content: openstack image create mobiledgex-v4.1.2 --disk-format qcow2 --container-format
+    bare --shared --file mobiledgex-v4.1.2.qcow2"""
+
+manifest_3 = """- id: 3
+  title: Download the manifest template
+  contenttype: code
+  contentsubtype: yaml
+  content: """
+
+manifest_3_private_key = ".*BEGIN RSA PRIVATE KEY[\s\S]{100,}END RSA PRIVATE KEY"  # key with 100+ chars
+manifest_3_crm_access_key = ".*path: /root/accesskey/accesskey.pem[\s\S]*-----BEGIN PRIVATE KEY-----[\s\S]{20,}[\s\S]{100,}"
