20e54d0b874bbff7d4363c6611f4cb48f07b2644
Andy Anderson <andy.anderson@mobiledgex.com>
2021-02-21 16:00:43 -0600

    change slack report to use blocks with links to jira 	modified:   slackJiraReport.py

--- jenkins/slackJiraReport.py
+++ jenkins/slackJiraReport.py
@@ -59,6 +59,53 @@ logging.basicConfig(
 logging.getLogger('urllib3').setLevel(logging.ERROR)
 logging.getLogger('zapi').setLevel(logging.DEBUG)
 
+#def build_report_blocks(project_id, version, cycle_name, cycle_summary=None, folder_name=None):
+def build_report_blocks():
+    report_url = f'https://mobiledgex.atlassian.net/plugins/servlet/ac/com.thed.zephyr.je/general-executions-enav?project.id={project_id}6#!view=list&offset=0&zql=project%20%3D%20%22edge-cloud%20QA%22%20AND%20fixVersion%20%3D%20%22{version_name}%22%20AND%20cycleName%20in%20(%22{cycle_name}%22)'
+    if folder_name:
+        report_url += f'%20AND%20folderName%20in%20(%22{folder_name}%22)'
+    status_url = report_url + '%20AND%20executionStatus%20%3D%20'
+    fail_url = status_url + 'FAIL'
+    pass_url = status_url + 'PASS'
+    wip_url = status_url + 'WIP'
+    blocked_url = status_url + 'BLOCKED'
+    unexecuted_url = status_url + 'UNEXECUTED'
+
+    if folder_name:
+        title = f'Automation {folder_name} Report for {cycle_name}'
+    else:
+        title = f'Automation Summary Report for {cycle_name}'
+
+    total_string = f'*Total TCs:* <{report_url}|{total_counted}>'
+    pass_string =  f'*Total Passed:* <{pass_url}|{total_pass}>   {pass_perc}%'
+    fail_string =  f'*Total Failed:* <{fail_url}|{total_fail}>   {fail_perc}%'
+    if not cycle_summary: fail_string += f'\\n*Total Failed w/bugs:* {total_fail_bugs}   {fail_bugs_perc}%'
+    if not cycle_summary: fail_string += f'\\n*Total Failed wo/bugs:* {total_fail_nobugs}   {fail_nobugs_perc}%'
+    unexec_string =  f'*Total Unexec:* <{unexecuted_url}|{total_unexecuted}>   {unexec_perc}%'
+    wip_string =     f'*Total WIP:* <{wip_url}|{total_wip}>   {wip_perc}%'
+    blocked_string = f'*Total Blocked:* <{blocked_url}|{total_blocked}>   {blocked_perc}%'
+       
+    block = f'''[
+                  {{
+                     "type": "header",
+                     "text": {{
+                        "type": "plain_text",
+                        "text": "{title}"
+                     }}
+                  }},
+                  {{
+                     "type": "section",
+                     "text": {{
+                        "type": "mrkdwn",
+                        "text": "{total_string}\\n{pass_string}\\n{fail_string}\\n{unexec_string}\\n{wip_string}\\n{blocked_string}"
+
+			}}
+		}}]'''
+
+    print('block', block)
+
+    return block
+
 z = zapi.Zapi(username=accountid, access_key=access_key, secret_key=secret_key, debug=True)
 j = jiraapi.Jiraapi(username=username, token=jira_token)
 
@@ -205,13 +252,6 @@ else:
     
 print('totalcount', total_count, 'totalcounted', total_counted, 'pass', total_pass, 'fail', total_fail, 'unexec', total_unexecuted)
 
-#total_counted = 1
-report_string = ''
-#report_string = f'*Cycle:*\t{cycle_name}\n\n'
-report_string += f'*Automation Report for'
-if folder_name:
-    report_string += f' {folder_name}'
-
 pass_perc = 0
 fail_perc = 0
 fail_bugs_perc = 0
@@ -232,88 +272,44 @@ if total_counted > 0:
     na_perc = round((total_na/total_counted)*100, 2)
     wontexec_perc = round((total_wontexec/total_counted)*100, 2)
 
-report_string += f' {cycle_name}*\n\n'
-report_string += f'>*Total TCs:* {total_counted}\n'
-report_string += f'>*Total Passed:* {total_pass}   {pass_perc}%\n'
-report_string += f'>*Total Failed:* {total_fail}   {fail_perc}%\n'
-if not cycle_summary: report_string += f'>*Total Failed w/bugs:* {total_fail_bugs}   {fail_bugs_perc}%\n'
-if not cycle_summary: report_string += f'>*Total Failed wo/bugs:* {total_fail_nobugs}   {fail_nobugs_perc}%\n'
-report_string += f'>*Total Unexec:* {total_unexecuted}   {unexec_perc}%\n'
-report_string += f'>*Total WIP:* {total_wip}   {wip_perc}%\n'
-report_string += f'>*Total Blocked:* {total_blocked}   {blocked_perc}%\n'
-#report_string += f'>*Total NA:* {total_na}   {na_perc}%\n'  # dont think jira reports this
-#report_string += f'>*Total WontExec:* {total_wontexec}   {wontexec_perc}%\n'  # dont think jira reports this
-if job_duration > 0:
-    report_string += f'>*Execution Time:* {round(job_duration/1000/60/60, 2)} hrs\n'
+#report_string = build_report_string()
+report_blocks = build_report_blocks()
 
-if total_count != total_counted:
-    report_string += f'*WARNING - total count did not add up. counted={total_counted} expected={total_count}*\n'
-if (total_pass + total_fail + total_unexecuted + total_wip + total_blocked + total_na + total_wontexec) != total_counted:
-    report_string += f'*WARNING - sum of exectution types did not add up. counted={total_pass + total_fail + total_unexecuted + total_wip + total_blocked + total_na + total_wontexec} expected={total_counted}*\n'
-summary_string = report_string
-
-if not summary_only and not cycle_summary:
-    if len(failed_nobugs_string) == 0:
-        failed_nobugs_string = '>None\n'
-    if len(failed_bugs_string_bytestcase) == 0:
-        failed_bugs_string_bytestcase = '>None\n'
-    if len(failed_bugs_string_bybug) == 0:
-        failed_bugs_string_bybug = '>None\n'
-    if len(unexecuted_string) == 0:
-        unexecuted_string = '>None\n'
-
-    
-    report_string += '>\n*Failed testcases without bugs:*\n' + failed_nobugs_string
-    report_string += '>\n*Failed testcases with bugs by testcase:*\n' + failed_bugs_string_bytestcase
-    report_string += '>\n*Failed testcases with bugs by bug:*\n'
-    if bug_dict:
-        for bug in bug_dict:
-            report_string += '>' + bug + '\n'
-            for tc in bug_dict[bug]:
-                report_string += '>      ' + tc + '\n'
-    else:
-        report_string += '>None\n'
-    report_string += '>\n*Unexecuted testcases:*\n' + unexecuted_string
-    #print(failed_bugs_string_bybug)
-
-if report_warning:
-    report_string += '\n' + report_warning
-    
 #print(report_string)
 
 #sys.exit(1)
 
-report_attachment = json.dumps(
-    [
-        {
-            #'pretext':'Automation Report For ' + cycle_name,
-            'text': '',
-            'title':'Automation Report For ' + cycle_name,
-            'fields': [
-                {
-                    'title':'Summary',
-                    'value': summary_string
-                },
-                {
-                    'title':'Failed testcases without bugs:',
-                    'value': failed_nobugs_string
-                },
-                {
-                    'title':'Failed testcases with bugs by testcase:',
-                    'value': failed_bugs_string_bytestcase
-                },
-                {
-                    'title':'Failed testcases with bugs by bug:',
-                    'value': failed_bugs_string_bybug
-                },
-                {
-                    'title':'Unexecuted testcases:',
-                    'value': unexecuted_string
-                }
-
-            ]
-        }
-    ])    
+#report_attachment = json.dumps(
+#    [
+#        {
+#            #'pretext':'Automation Report For ' + cycle_name,
+#            'text': '',
+#            'title':'Automation Report For ' + cycle_name,
+#            'fields': [
+#                {
+#                    'title':'Summary',
+#                    'value': summary_string
+#                },
+#                {
+#                    'title':'Failed testcases without bugs:',
+#                    'value': failed_nobugs_string
+#                },
+#                {
+#                    'title':'Failed testcases with bugs by testcase:',
+#                    'value': failed_bugs_string_bytestcase
+#                },
+#                {
+#                    'title':'Failed testcases with bugs by bug:',
+#                    'value': failed_bugs_string_bybug
+#                },
+#                {
+#                    'title':'Unexecuted testcases:',
+#                    'value': unexecuted_string
+#                }
+#
+#            ]
+#        }
+#    ])    
 
 #sc.api_call(
 #    "chat.postMessage",
@@ -323,12 +319,22 @@ report_attachment = json.dumps(
 #    #attachments=report_attachment 
 #)
 
-print(report_string)
 
+
+blocks = '''[
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*Total TCs*: 1823\n*Total Passed*: <https://mobiledgex.atlassian.net/plugins/servlet/ac/com.thed.zephyr.je/general-executions-enav?project.id=10006#!view=list&offset=0&zql=project%20%3D%20%22edge-cloud%20QA%22%20AND%20fixVersion%20%3D%20%22CirrusR3%22%20AND%20cycleName%20in%20(%222021-02-20_CirrusR3%22)%20AND%20folderName%20in%20(%22dme%22)%20AND%20executionStatus%20%3D%20FAIL|1410>   77.35%"
+			}
+		}]'''
 if slack:
     print('slacking report')
     try:
-        response = sc.chat_postMessage(channel='#qa-automation', text=report_string)
+        #response = sc.chat_postMessage(channel='#qa-automation', text=report_string)
+        response = sc.chat_postMessage(channel='#qa-automation', blocks=report_blocks)
+
         print('slack message:', response)
         if 'response_metadata' in response and response['response_metadata']['messages']:
             response = sc.chat_postMessage(channel='#qa-automation', text=response['response_metadata']['messages'])
@@ -337,4 +343,59 @@ if slack:
         assert e.response["ok"] is False
         assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
         print(f"Got an error: {e.response['error']}")
+
+    
+def build_report_string():
+    report_string = ''
+    #report_string = f'*Cycle:*\t{cycle_name}\n\n'
+    report_string += f'*Automation Report for'
+    if folder_name:
+        report_string += f' {folder_name}'
+
+    report_string += f' {cycle_name}*\n\n'
+    report_string += f'>*Total TCs:* {total_counted}\n'
+    report_string += f'>*Total Passed:* {total_pass}   {pass_perc}%\n'
+    report_string += f'>*Total Failed:* {total_fail}   {fail_perc}%\n'
+    if not cycle_summary: report_string += f'>*Total Failed w/bugs:* {total_fail_bugs}   {fail_bugs_perc}%\n'
+    if not cycle_summary: report_string += f'>*Total Failed wo/bugs:* {total_fail_nobugs}   {fail_nobugs_perc}%\n'
+    report_string += f'>*Total Unexec:* {total_unexecuted}   {unexec_perc}%\n'
+    report_string += f'>*Total WIP:* {total_wip}   {wip_perc}%\n'
+    report_string += f'>*Total Blocked:* {total_blocked}   {blocked_perc}%\n'
+    #report_string += f'>*Total NA:* {total_na}   {na_perc}%\n'  # dont think jira reports this
+    #report_string += f'>*Total WontExec:* {total_wontexec}   {wontexec_perc}%\n'  # dont think jira reports this
+    if job_duration > 0:
+        report_string += f'>*Execution Time:* {round(job_duration/1000/60/60, 2)} hrs\n'
+
+    if total_count != total_counted:
+        report_string += f'*WARNING - total count did not add up. counted={total_counted} expected={total_count}*\n'
+    if (total_pass + total_fail + total_unexecuted + total_wip + total_blocked + total_na + total_wontexec) != total_counted:
+        report_string += f'*WARNING - sum of exectution types did not add up. counted={total_pass + total_fail + total_unexecuted + total_wip + total_blocked + total_na + total_wontexec} expected={total_counted}*\n'
+    summary_string = report_string
+
+    if not summary_only and not cycle_summary:
+        if len(failed_nobugs_string) == 0:
+            failed_nobugs_string = '>None\n'
+        if len(failed_bugs_string_bytestcase) == 0:
+            failed_bugs_string_bytestcase = '>None\n'
+        if len(failed_bugs_string_bybug) == 0:
+            failed_bugs_string_bybug = '>None\n'
+        if len(unexecuted_string) == 0:
+            unexecuted_string = '>None\n'
+
+    
+        report_string += '>\n*Failed testcases without bugs:*\n' + failed_nobugs_string
+        report_string += '>\n*Failed testcases with bugs by testcase:*\n' + failed_bugs_string_bytestcase
+        report_string += '>\n*Failed testcases with bugs by bug:*\n'
+        if bug_dict:
+            for bug in bug_dict:
+                report_string += '>' + bug + '\n'
+                for tc in bug_dict[bug]:
+                    report_string += '>      ' + tc + '\n'
+        else:
+            report_string += '>None\n'
+        report_string += '>\n*Unexecuted testcases:*\n' + unexecuted_string
+        #print(failed_bugs_string_bybug)
+
+    if report_warning:
+        report_string += '\n' + report_warning
 
