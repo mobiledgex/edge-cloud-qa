3a98a259f808137def360b26094a55d95d169ed3
Andy Anderson <andy.anderson@mobiledgex.com>
2021-03-16 19:44:55 -0500

    change server_ping_threaded vm md5 	modified:   ../../config/automation_variables.py

--- testcases/config/automation_variables.py
+++ testcases/config/automation_variables.py
@@ -54,7 +54,7 @@ docker_image_porttest = 'docker-qa.mobiledgex.net/mobiledgex/images/port_test_se
 
 artifactory_dummy_image_name = 'execJira.py'
 
-qcow_centos_image = 'https://artifactory-qa.mobiledgex.net/artifactory/repo-MobiledgeX/server_ping_threaded_centos7.qcow2#md5:5ce8dbcdd8b7c2054779d742f4bf602d'
+qcow_centos_image = 'https://artifactory-qa.mobiledgex.net/artifactory/repo-MobiledgeX/server_ping_threaded_centos7.qcow2#md5:8d8f9c268fd419b16084c9ba054b483a'
 qcow_centos_image_notrunning = 'https://artifactory-qa.mobiledgex.net/artifactory/repo-MobiledgeX/server_ping_threaded_notrunning_centos7.qcow2#md5:7a08091f71f1e447ce291e467cc3926c'
 qcow_windows_image = 'https://artifactory-qa.mobiledgex.net/artifactory/repo-MobiledgeX/server_ping_threaded_windows2012.qcow2#md5:42171406daca80298098ac314200634a'
 qcow_centos_openstack_image = 'server_ping_threaded_centos7'
