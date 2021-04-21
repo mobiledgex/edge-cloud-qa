93ec7b9f6c31483412b06ffa4417e1c39323a037
dunkletx <63373223+dunkletx@users.noreply.github.com>
2021-04-21 10:28:28 -0500

    updated cloudletverification files timeout=${s_timeout} fixed tag shared should have been dedicated in metrics robot file updated vcd2 variable file with envvar vcd-nsx-type removed MEX_EXTERNAL_NETWORK_GATEWAY not needed and added physicalname for oauth updated vsphere variable file added cloudlet_rootlb parameters created wwt variable file to test NSX-T vcd cloudlet

--- testcases/cloudletverification/cloudletverification_wwt_vars.py
+++ testcases/cloudletverification/cloudletverification_wwt_vars.py
@@ -38,19 +38,19 @@ developer_organization_name = 'MobiledgeX'
 
 # test will create flavors or not.  Requires mexadmin username/password.  Sometimes flavors will be predefined and we wont have permissions to create our own
 create_flavors = True 
-# mcctl command to create this cloudlet no longer need envvar=MEX_EXTERNAL_NETWORK_GATEWAY=139.178.87.225 and now using different pyhisicalname for oauth testing qa2-lab-oauth
-# date;time mcctl --addr https://console-qa.mobiledgex.net:443 region --skipverify CreateCloudlet cloudlet=automation-qa2-vcd-01 region=US cloudlet-org=packet physicalname=qa2-lab-oauth infraapiaccess=DirectAccess platformtype=PlatformTypeVcd location.latitude=33 location.longitude=-96 numdynamicips=10 envvar=MEX_VDC_TEMPLATE=mobiledgex-v4.3.3-vcd envvar=MEX_CATALOG=qa2-cat envvar=MEX_DATASTORE=datastore1 envvar=MEX_EXTERNAL_NETWORK_MASK=28 envvar=MEX_EXTERNAL_NETWORK_GATEWAY=139.178.87.225 envvar=MEX_NETWORK_SCHEME=cidr=10.102.X.0/24 envvar=MEX_IMAGE_DISK_FORMAT=vmdk deployment=docker infraconfig.flavorname=vcd-medium envvar=MEX_EXT_NETWORK=external-network-qa2 envvar=MEX_CLOUDLET_FIREWALL_WHITELIST_EGRESS='protocol=tcp,portrange=1:65535,remotecidr=0.0.0.0/0;protocol=udp,portrange=1:65535,remotecidr=0.0.0.0/0;protocol=icmp,remotecidr=0.0.0.0/0;protocol=tcp,portrange=22,remotecidr=76.184.227.212/32;protocol=tcp,portrange=22,remotecidr=35.199.188.102/32' envvar=MEX_CLOUDLET_FIREWALL_WHITELIST_INGRESS='protocol=tcp,portrange=1:65535,remotecidr=0.0.0.0/0;protocol=udp,portrange=1:65535,remotecidr=0.0.0.0/0;protocol=icmp,remotecidr=0.0.0.0/0;protocol=tcp,portrange=22,remotecidr=76.184.227.212/32;protocol=tcp,portrange=22,remotecidr=35.199.188.102/32' --debug
+# mcctl command to create this cloudlet
+# date;time mcctl --addr https://console-qa.mobiledgex.net:443 region --skipverify CreateCloudlet cloudlet=qawwt-cld1 region=US cloudlet-org=wwt physicalname=qawwt-lab infraapiaccess=DirectAccess platformtype=PlatformTypeVcd location.latitude=35 location.longitude=-95 numdynamicips=64 envvar=MEX_VDC_TEMPLATE=mobiledgex-v4.2.1-vcd envvar=MEX_CATALOG=MobiledgeX envvar=MEX_EXTERNAL_NETWORK_MASK=25 envvar=MEX_EXTERNAL_NETWORK_GATEWAY=10.253.172.129 envvar=MEX_NETWORK_SCHEME=cidr=10.102.X.0/24 envvar=MEX_IMAGE_DISK_FORMAT=vmdk deployment=docker infraconfig.flavorname=vcd-medium envvar=MEX_EXT_NETWORK=vcd-net-1  --debug
 
 
 # env_vars=${cloudlet_env_vars}
 # vcd qaorg cloudlet variables
-operator_name = 'packet'
+operator_name = 'wwt'
 cloudlet_platform_type = 'PlatformTypeVcd'
-cloudlet_latitude = '33'
-cloudlet_longitude = '-96'
+cloudlet_latitude = '35'
+cloudlet_longitude = '-95'
 cloudlet_ipsupport = 'dynamic'
 cloudlet_infraapiaccess = 'directaccess'
-cloudlet_numdynamicips = '10'
+cloudlet_numdynamicips = '60'
 cloudlet_security_group = 'cloudletverification'
 cloudlet_external_subnet = 'external-subnet'  # used for cloudlet metrics verification
 cloudlet_infraconfig_flavorname = 'vcd-medium'
@@ -61,27 +61,26 @@ deployment = 'docker'
 cloudlet_rootlb_ram = 'MEX_SHARED_ROOTLB_RAM=4096'
 cloudlet_rootlb_disk = 'MEX_SHARED_ROOTLB_DISK=42'
 cloudlet_rootlb_vcpus = 'MEX_SHARED_ROOTLB_VCPUS=4'
-cloudlet_nsx_type = 'VCD_NSX_TYPE=NSX-V'
 
 
-# for VCD qa2org /28
-cloudlet_name = 'automation-qa2-vcd-01'
-#physical_name = 'qa2-lab' pre oauth testing and secret does not have oauth use this for negative testing
-physical_name = 'qa2-lab-oauth'
+# for VCD mex-edge /25
+cloudlet_name = 'qawwt-cld1'
+physical_name = 'qawwt-lab'
 cloudlet_vdc_template = 'MEX_VDC_TEMPLATE=mobiledgex-v4.3.2-vcd'
-cloudlet_mex_catalog =  'MEX_CATALOG=qa2-cat'
+cloudlet_mex_catalog =  'MEX_CATALOG=MobiledgeX'
 cloudlet_whitelist_in = 'MEX_CLOUDLET_FIREWALL_WHITELIST_EGRESS=protocol=tcp,portrange=1:65535,remotecidr=0.0.0.0/0;protocol=udp,portrange=1:65535,remotecidr=0.0.0.0/0;protocol=icmp,remotecidr=0.0.0.0/0;protocol=tcp,portrange=22,remotecidr=76.184.227.212/32;protocol=tcp,portrange=22,remotecidr=35.199.188.102/32'
 cloudlet_whitelist_eg = 'MEX_CLOUDLET_FIREWALL_WHITELIST_INGRESS=protocol=tcp,portrange=1:65535,remotecidr=0.0.0.0/0;protocol=udp,portrange=1:65535,remotecidr=0.0.0.0/0;protocol=icmp,remotecidr=0.0.0.0/0;protocol=tcp,portrange=22,remotecidr=76.184.227.212/32;protocol=tcp,portrange=22,remotecidr=35.199.188.102/32'
 cloudlet_datastore = 'MEX_DATASTORE=datastore1'
-cloudlet_ext_netmask = 'MEX_EXTERNAL_NETWORK_MASK=28'
-#cloudlet_ext_gateway = 'MEX_EXTERNAL_NETWORK_GATEWAY=139.178.87.225'
-cloudlet_ext_network = 'MEX_EXT_NETWORK=external-network-qa2'
+cloudlet_ext_netmask = 'MEX_EXTERNAL_NETWORK_MASK=25'
+cloudlet_ext_gateway = 'MEX_EXTERNAL_NETWORK_GATEWAY=10.253.172.129'
+cloudlet_ext_network = 'MEX_EXT_NETWORK=vcd-net-1'
 cloudlet_image_disk_format = 'MEX_IMAGE_DISK_FORMAT=vmdk'
 cloudlet_ext_netscheme = 'MEX_NETWORK_SCHEME=cidr=10.102.X.0/24'
+cloudlet_nsx_type = 'VCD_NSX_TYPE=NSX-T'
 
 #vcd format
-#removing envvar=MEX_EXTERNAL_NETWORK_GATEWAY=139.178.87.225 cloudlet_ext_gateway
-cloudlet_env_vars= f'{cloudlet_datastore},{cloudlet_rootlb_ram},{cloudlet_rootlb_disk},{cloudlet_rootlb_vcpus},{cloudlet_ext_netmask},{cloudlet_ext_netscheme},{cloudlet_ext_network},{cloudlet_image_disk_format},{cloudlet_vdc_template},{cloudlet_mex_catalog},{cloudlet_whitelist_in},{cloudlet_whitelist_eg},{cloudlet_nsx_type}'
+
+cloudlet_env_vars= f'{cloudlet_datastore},{cloudlet_rootlb_ram},{cloudlet_rootlb_disk},{cloudlet_rootlb_vcpus},{cloudlet_ext_netmask},{cloudlet_ext_netscheme},{cloudlet_ext_network},{cloudlet_ext_gateway},{cloudlet_image_disk_format},{cloudlet_vdc_template},{cloudlet_mex_catalog}, {cloudlet_nsx_type}'
 
 # docker image used for docker/k8s deployments
 docker_image = 'docker-qa.mobiledgex.net/mobiledgex/images/server_ping_threaded:6.0'
@@ -90,7 +89,8 @@ docker_image_gpu = 'docker-qa.mobiledgex.net/mobiledgex/images/mobiledgexsdkdemo
 docker_image_trustpolicy = 'docker-qa.mobiledgex.net/mobiledgex/images/port_test_server:1.0'
 
 # QCOW image used for VM deployments which has the test app running on it
-qcow_centos_image = 'https://artifactory-qa.mobiledgex.net/artifactory/repo-MobiledgeX/server_ping_threaded_centos7_http.qcow2#md5:c7f7e312dd18b1c9ea586650721c75ba'
+#qcow_centos_image = 'https://artifactory-qa.mobiledgex.net/artifactory/repo-MobiledgeX/server_ping_threaded_centos7_http.qcow2#md5:c7f7e312dd18b1c9ea586650721c75ba'
+qcow_centos_image = 'https://artifactory-qa.mobiledgex.net/artifactory/repo-MobiledgeX/server_ping_threaded_centos7.qcow2#md5:8d8f9c268fd419b16084c9ba054b483a'
 # QCOW image used for VM deployments which does NOT have the test app running on it. This is used for cloudconfig tests with starts the app via a manifest file
 qcow_centos_image_notrunning = 'https://artifactory-qa.mobiledgex.net/artifactory/repo-MobiledgeX/server_ping_threaded_notrunning_centos7.qcow2#md5:7a08091f71f1e447ce291e467cc3926c'
 # QCOW image used for GPU testing. This has the openpose app installed
