6059c4113decf02d616c872accdae8abbd449616
Andy Anderson <andy.anderson@mobiledgex.com>
2021-09-30 15:22:11 -0500

    cert change for grpc dme based on some letsencrypt change 	new file:   ../certs/isrgrootx1.pem 	modified:   mex_grpc.py

--- modules/mex_grpc.py
+++ modules/mex_grpc.py
@@ -3,11 +3,12 @@ import sys
 import os
 import grpc
 
-logging.basicConfig(format='%(asctime)s %(levelname)s %(funcName)s line:%(lineno)d - %(message)s',datefmt='%d-%b-%y %H:%M:%S')
+logging.basicConfig(format='%(asctime)s %(levelname)s %(funcName)s line:%(lineno)d - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 logger = logging.getLogger(__name__)
 
-#export GRPC_VERBOSITY=DEBUG
-#export GRPC_TRACE=client_channel,channel,health_check_client,http,http2_stream_state
+# export GRPC_VERBOSITY=DEBUG
+# export GRPC_TRACE=client_channel,channel,health_check_client,http,http2_stream_state
+
 
 class MexGrpc(object):
     def __init__(self, address, root_cert=None, key=None, client_cert=None):
@@ -22,10 +23,10 @@ class MexGrpc(object):
 
             with open(root_cert_real, 'rb') as f:
                 logger.debug('using root_cert=' + root_cert_real)
-                #trusted_certs = f.read().encode()
+                # trusted_certs = f.read().encode()
                 trusted_certs = f.read()
-            with open(key_real,'rb') as f:
-                logger.debug('using key='+key_real)
+            with open(key_real, 'rb') as f:
+                logger.debug('using key=' + key_real)
                 trusted_key = f.read()
             with open(client_cert_real, 'rb') as f:
                 logger.debug('using client cert=' + client_cert_real)
@@ -33,16 +34,22 @@ class MexGrpc(object):
 
             # create credentials
             credentials = grpc.ssl_channel_credentials(root_certificates=trusted_certs, private_key=trusted_key, certificate_chain=cert_chain)
-            channel_options = [('grpc.keepalive_time_ms',600000),  # 3mins. seems I cannot do less than 5mins. does 5mins anyway if set lower
+            channel_options = [('grpc.keepalive_time_ms', 600000),  # 3mins. seems I cannot do less than 5mins. does 5mins anyway if set lower
                                ('grpc.keepalive_timeout_ms', 5000),
                                ('grpc.http2.min_time_between_pings_ms', 60000),
                                ('grpc.http2.max_pings_without_data', 0),
                                ('grpc.keepalive_permit_without_calls', 1)]
             self.grpc_channel = grpc.secure_channel(address, credentials, options=channel_options)
         else:
-            #self.grpc_channel = grpc.insecure_channel(address)
-            credentials = grpc.ssl_channel_credentials()
-            channel_options = [('grpc.keepalive_time_ms',600000),  # 3mins. seems I cannot do less than 5mins. does 5mins anyway if set lower
+            # self.grpc_channel = grpc.insecure_channel(address)
+            # added this after some letsencrypt cert change. Not sure why I need to load this
+            root_cert_real = self._findFile('isrgrootx1.pem')
+            with open(root_cert_real, 'rb') as f:
+                logger.debug('using root_cert=' + root_cert_real)
+                trusted_certs = f.read()
+            credentials = grpc.ssl_channel_credentials(root_certificates=trusted_certs)
+            # credentials = grpc.ssl_channel_credentials()
+            channel_options = [('grpc.keepalive_time_ms', 600000),  # 3mins. seems I cannot do less than 5mins. does 5mins anyway if set lower
                                ('grpc.keepalive_timeout_ms', 5000),
                                ('grpc.http2.min_time_between_pings_ms', 60000),
                                ('grpc.http2.max_pings_without_data', 0),
