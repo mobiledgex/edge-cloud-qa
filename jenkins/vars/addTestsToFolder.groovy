4b3667548bfa104b5694eebc781c6824e72c9e41
Andy Anderson <andy.anderson@mobiledgex.com>
2021-12-12 17:52:27 -0600

    update jenkins shared lib 	modified:   addTestsToFolder.groovy 	new file:   deployEdgeCloud.groovy 	modified:   slackMessage.groovy

--- jenkins/vars/addTestsToFolder.groovy
+++ jenkins/vars/addTestsToFolder.groovy
@@ -1,4 +1,4 @@
-def call(version, project, cycle, folder, components=null, components_omit=null) {
+def addTestsToFolder(version, project, cycle, folder, components=null, components_omit=null) {
     def status = -1
     def s = 'export PYTHONPATH=$WORKSPACE/go/src/github.com/mobiledgex/modules;python3 ./copyTestsToRelease.py --version ' + version + ' --project ' + project + ' --cycle ' + cycle +  ' --folder ' + folder
     if (components != null) {
@@ -16,3 +16,47 @@ def call(version, project, cycle, folder, components=null, components_omit=null)
     }
 }
 
+def call(version, project, cycle) {
+    dir('go/src/github.com/mobiledgex/jenkins') {
+        def add_pre = 'export PYTHONPATH=$WORKSPACE/go/src/github.com/mobiledgex/modules;./createCycleAddTestcases.py --version ' + params.Version + ' --project ' + params.Project + ' --cycle ' + cycle
+        echo "createcycle build resutl ${currentBuild.result}"
+        echo "addpre ${add_pre}"
+
+        sh add_pre    
+                    
+        sh add_pre + ' --folder controller'
+        addTestsToFolder(params.Version, params.Project, cycle, 'controller', 'Automated,Controller', null)
+                    
+        sh add_pre + ' --folder mastercontroller'
+        addTestsToFolder(params.Version, params.Project, cycle, 'mastercontroller', 'Automated,MasterController', null)
+        addTestsToFolder(params.Version, params.Project, cycle, 'mastercontroller', 'Automated,Mcctl', null)
+
+        sh add_pre + ' --folder dme'
+        addTestsToFolder(params.Version, params.Project, cycle, 'dme', 'Automated,DME', 'SDK,Metrics,Performance')
+
+        sh add_pre + ' --folder sdk'
+        addTestsToFolder(params.Version, params.Project, cycle, 'sdk', 'Automated,SDK', null)
+                   
+        sh add_pre + ' --folder openstack'
+        addTestsToFolder(params.Version, params.Project, cycle, 'openstack', 'Automated,CRM,Openstack', null)
+
+        sh add_pre + ' --folder vsphere'
+        addTestsToFolder(params.Version, params.Project, cycle, 'vsphere', 'Automated,CRM,Vsphere', null)
+                    
+        sh add_pre + ' --folder vcd'
+        addTestsToFolder(params.Version, params.Project, cycle, 'vcd', 'Automated,CRM,VCD', null)
+                    
+        sh add_pre + ' --folder anthos'
+        addTestsToFolder(params.Version, params.Project, cycle, 'anthos', 'Automated,CRM,Anthos', null)
+
+        sh add_pre + ' --folder metrics'
+        addTestsToFolder(params.Version, params.Project, cycle, 'metrics', 'Automated,Metrics', null)
+
+        sh add_pre + ' --folder performance/security'
+        addTestsToFolder(params.Version, params.Project, cycle, 'performance/security', 'Automated,Performance', null)
+        addTestsToFolder(params.Version, params.Project, cycle, 'performance/security', 'Automated,Security', null)
+                    
+        sh add_pre + ' --folder webui'
+        addTestsToFolder(params.Version, params.Project, cycle, 'webui', 'Automated,WebUI', null)
+    }
+}
