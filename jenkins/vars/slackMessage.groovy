981d1cd95e5e50dc55c3f6460b558e54d8dbf6fa
Andy Anderson <andy.anderson@mobiledgex.com>
2021-12-12 18:00:44 -0600

    update jenkins shared lib 	modified:   slackMessage.groovy

--- jenkins/vars/slackMessage.groovy
+++ jenkins/vars/slackMessage.groovy
@@ -1,14 +1,19 @@
-slackurl = 'https://mobiledgex.slack.com/services/hooks/jenkins-ci/'
-slackcred = 'cdda7d70-b701-4983-b287-e3f46dde02e9'
-slackchannel = '#qa-automation'
+import groovy.transform.Field
+
+@Field slackurl = 'https://mobiledgex.slack.com/services/hooks/jenkins-ci/'
+@Field slackcred = 'cdda7d70-b701-4983-b287-e3f46dde02e9'
+@Field slackchannel = '#qa-automation'
 
 def good(message) {
+    print('good')
     slackSend baseUrl: slackurl, tokenCredentialId: slackcred, channel: slackchannel, color: "good", message: message
 }
 def fail(message) {
+    print('fail')
     slackSend baseUrl: slackurl, tokenCredentialId: slackcred, channel: slackchannel, color: "danger", message: message
 }
 def warning(message) {
+    print('warning')
     slackSend baseUrl: slackurl, tokenCredentialId: slackcred, channel: slackchannel, color: "warning", message: message
 }
 
