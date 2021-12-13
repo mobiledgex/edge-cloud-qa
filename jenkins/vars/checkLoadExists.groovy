07d7aedd1cf6541a5ab32984763031ee0217b90a
Andy Anderson <andy.anderson@mobiledgex.com>
2021-12-13 12:14:44 -0600

    add checkLoadExists for jenkins shared lib 	new file:   checkLoadExists.groovy

---
+++ jenkins/vars/checkLoadExists.groovy
@@ -0,0 +1,24 @@
+def call(dateValue) {
+    date = new Date()
+    sdate = new SimpleDateFormat("yyy-MM-dd")
+    Calendar cal = Calendar.getInstance()
+                
+    if(params.BuildDate == "today") {
+        dateValue = sdate.format(date)
+    } else {
+        dateValue = params.BuildDate
+    }
+    echo dateValue
+
+    cycle = dateValue + '_' + params.Version
+    currentBuild.displayName = cycle
+
+    slackMessage.good('Starting regression for ' + cycle)
+
+    def s = 'curl -s https://robot\\$qa:RIuSwsdA9naFkunkDtg7TrPOHG3YoqS8@harbor.mobiledgex.net/v2/mobiledgex/edge-cloud/tags/list | jq ".tags | index(\\"' + dateValue + '\\")"'
+    def index  = sh(script: s, returnStdout: true);
+    if(index == 'null') {
+        println "${s} failed"
+        currentBuild.result = 'FAILURE'
+    }
+}
