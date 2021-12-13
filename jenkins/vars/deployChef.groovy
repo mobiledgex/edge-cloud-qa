624ce3ea382746f9eec3e04d1b52937d7283327b
Andy Anderson <andy.anderson@mobiledgex.com>
2021-12-13 14:23:14 -0600

    update chefDeploy 	modified:   deployChef.groovy

--- jenkins/vars/deployChef.groovy
+++ jenkins/vars/deployChef.groovy
@@ -2,7 +2,7 @@ import groovy.transform.Field
 
 @Field gitcred = '79b116ea-d7ac-4d6c-928d-49b79e5f9bef'
 
-def call() {
+def call(dateValue) {
     dir('go/src/github.com/mobiledgex/edge-cloud-infra') {
         sh 'rm -rf $WORKSPACE/go/src/github.com/mobiledgex/edge-cloud-infra/*'
         checkout([$class: 'GitSCM',
@@ -16,4 +16,9 @@ def call() {
             userRemoteConfigs: [[credentialsId: gitcred,
             url: 'https://github.com/mobiledgex/edge-cloud-infra.git']]])
     }
+
+    dir('go/src/github.com/mobiledgex/edge-cloud-infra/chef/policyfiles') {
+        sh "echo \"override['qa']['edgeCloudVersion'] = '" + dateValue + "'\" >> docker_crm.rb && rm -f docker_crm.lock.json && chef install docker_crm.rb "
+    }
+
 }
