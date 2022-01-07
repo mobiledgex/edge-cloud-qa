d579c3078e3a60e98bf4d1b355f369606f6bbb16
Andy Anderson <andy.anderson@mobiledgex.com>
2022-01-07 13:58:09 -0600

    update and reformat RunCommand.py for accesscloudlet 	modified:   ../../../modules/mex_master_controller/RunCommand.py

--- modules/mex_master_controller/RunCommand.py
+++ modules/mex_master_controller/RunCommand.py
@@ -222,8 +222,8 @@ class RunCommand(MexOperation):
                 return dataqueue
         data = asyncio.get_event_loop().run_until_complete(wsconnect())
 
-        if 'Error' in data:
-            raise Exception(f'error={data}')
+        #if 'Error' in data:
+        #    raise Exception(f'error={data}')
          
         return data
 
