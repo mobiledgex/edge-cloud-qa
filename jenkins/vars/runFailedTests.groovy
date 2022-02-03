9c0292dd3ec64d9d67dbbb83cbb112d671989ef9
Andy Anderson <andy.anderson@mobiledgex.com>
2022-02-03 11:03:58 -0600

    update runFailedTests.groovy 	modified:   runFailedTests.groovy

--- jenkins/vars/runFailedTests.groovy
+++ jenkins/vars/runFailedTests.groovy
@@ -1,15 +1,35 @@
 def call(cycle) {
-//    stages {
-        stage('Run MC Failed Tests') {
-            build job: 'runTestcases', parameters: [
-                string(name: 'Components', value: 'Automated'),
-                string(name: 'Project', value: params.Project),
-                string(name: 'Cycle', value: cycle),
-                string(name: 'MasterController', value: params.MasterController),
-                string(name: 'TestTarget', value: 'MasterController'),
-                string(name: 'VariableFile', value: params.VariableFile),
-                string(name: 'NumberParallelExecutions', value: '1')]
-        }
-//    }
+    stage('MC') {
+        build job: 'runTestcases', parameters: [
+            string(name: 'Components', value: 'Automated'),
+            string(name: 'Project', value: params.Project),
+            string(name: 'Cycle', value: cycle),
+            string(name: 'MasterController', value: params.MasterController),
+            string(name: 'TestTarget', value: 'MasterController'),
+            string(name: 'VariableFile', value: params.VariableFile),
+            string(name: 'NumberParallelExecutions', value: '1')]
+    }
+
+    stage('Controller') {
+        build job: 'runTestcases', parameters: [
+            string(name: 'Components', value: 'Automated'),
+            string(name: 'Project', value: params.Project),
+            string(name: 'Cycle', value: cycle),
+            string(name: 'MasterController', value: params.MasterController),
+            string(name: 'TestTarget', value: 'Controller'),
+            string(name: 'VariableFile', value: params.VariableFile),
+            string(name: 'NumberParallelExecutions', value: '1')]
+    }
+
+    stage('DME') {
+        build job: 'runTestcases', parameters: [
+            string(name: 'Components', value: 'Automated'),
+            string(name: 'Project', value: params.Project),
+            string(name: 'Cycle', value: cycle),
+            string(name: 'MasterController', value: params.MasterController),
+            string(name: 'TestTarget', value: 'DME'),
+            string(name: 'VariableFile', value: params.VariableFile),
+            string(name: 'NumberParallelExecutions', value: '1')]
+    }
 }
 
