c7c62b860ae7f5e784327f5e68c9fc0aa98326cf
Andy Anderson <andy.anderson@mobiledgex.com>
2022-02-03 07:55:21 -0600

    add runFailedTests.groovy for rerunning failed tests 	new file:   runFailedTests.groovy

---
+++ jenkins/vars/runFailedTests.groovy
@@ -0,0 +1,15 @@
+def call(cycle) {
+    stages {
+        stage('Run MC Failed Tests') {
+            build job: 'runTestcases', parameters: [
+                string(name: 'Components', value: 'Automated'),
+                string(name: 'Project', value: params.Project),
+                string(name: 'Cycle', value: cycle),
+                string(name: 'MasterController', value: params.MasterController),
+                string(name: 'TestTarget', value: 'MasterController'),
+                string(name: 'VariableFile', value: params.VariableFile),
+                string(name: 'NumberParallelExecutions', value: '1')]
+        }
+    }
+}
+
