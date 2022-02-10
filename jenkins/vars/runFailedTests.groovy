01df8d68940161ae574281cd67b33e0719148bd9
Andy Anderson <andy.anderson@mobiledgex.com>
2022-02-10 14:33:58 -0600

    add options for running certain regression areas. fixed rerunfailed 	modified:   runRegression_declarative.jenkinsfile 	modified:   vars/runFailedTests.groovy

--- jenkins/vars/runFailedTests.groovy
+++ jenkins/vars/runFailedTests.groovy
@@ -1,43 +1,54 @@
 def call(cycle) {
     stage('MC') {
+        when { params.runMCTests == true }
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE', message: 'Rerun MC tests failed') {
-            build job: 'runTestcases', parameters: [
-                string(name: 'Components', value: 'Automated'),
+            build job: 'runMcTestcases', parameters: [
+                string(name: 'SlaveNode', value: params.SlaveNodeMasterController),
                 string(name: 'Project', value: params.Project),
                 string(name: 'Cycle', value: cycle),
                 string(name: 'MasterController', value: params.MasterController),
                 string(name: 'TestTarget', value: 'MasterController'),
                 string(name: 'VariableFile', value: params.VariableFile),
-                booleanParam(name: 'RunFailedOnly', value: true),
-                string(name: 'NumberParallelExecutions', value: '1')]
+                string(name: 'TestTimeout', value: params.TestTimeoutMasterController),
+                booleanParam(name: 'RunFailedOnly', value: '1')]
         }
     }
 
     stage('Controller') {
+        when { params.runControllerTests == true }
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE', message: 'Rerun Controller tests failed') {
-            build job: 'runTestcases', parameters: [
-                string(name: 'Components', value: 'Automated'),
+            build job: 'runControllerTestcases', parameters: [
+                string(name: 'SlaveNode', value: params.SlaveNodeController),
                 string(name: 'Project', value: params.Project),
                 string(name: 'Cycle', value: cycle),
+                string(name: 'Controller', value: params.Controller),
                 string(name: 'MasterController', value: params.MasterController),
+                string(name: 'DME', value: params.DME),
+                string(name: 'DMERest', value: params.DMERest),
+                string(name: 'DMERestCert', value: params.DMERestCert),
                 string(name: 'TestTarget', value: 'Controller'),
                 string(name: 'VariableFile', value: params.VariableFile),
-                booleanParam(name: 'RunFailedOnly', value: true),
-                string(name: 'NumberParallelExecutions', value: '1')]
+                string(name: 'TestTimeout', value: params.TestTimeoutController),
+                booleanParam(name: 'RunFailedOnly', value: '1')]
         }
     }
 
     stage('DME') {
+        when { params.runDMETests == true }
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE', message: 'Rerun DME tests failed') {
-            build job: 'runTestcases', parameters: [
-                string(name: 'Components', value: 'Automated'),
+            build job: 'runDmeTestcases', parameters: [
+                string(name: 'SlaveNode', value: params.SlaveNodeDME),
                 string(name: 'Project', value: params.Project),
                 string(name: 'Cycle', value: cycle),
+                string(name: 'Controller', value: params.Controller),
                 string(name: 'MasterController', value: params.MasterController),
+                string(name: 'DME', value: params.DME),
+                string(name: 'DMERest', value: params.DMERest),
+                string(name: 'DMERestCert', value: params.DMERestCert),
                 string(name: 'TestTarget', value: 'DME'),
                 string(name: 'VariableFile', value: params.VariableFile),
-                booleanParam(name: 'RunFailedOnly', value: true),
-                string(name: 'NumberParallelExecutions', value: '1')]
+                string(name: 'TestTimeout', value: params.TestTimeoutDME),
+                booleanParam(name: 'RunFailedOnly', value: '1')]
         }
     }
 }
