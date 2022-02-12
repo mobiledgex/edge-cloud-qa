f08ebf7764298350850d2caa776c9454ddf6c24e
Andy Anderson <andy.anderson@mobiledgex.com>
2022-02-12 10:06:10 -0600

    fix checkLoadExists 	modified:   checkLoadExists.groovy

--- jenkins/vars/checkLoadExists.groovy
+++ jenkins/vars/checkLoadExists.groovy
@@ -2,8 +2,10 @@ def call(dateValue) {
     def s = 'curl -s https://robot\\$qa:RIuSwsdA9naFkunkDtg7TrPOHG3YoqS8@harbor.mobiledgex.net/v2/mobiledgex/edge-cloud/tags/list | jq ".tags | index(\\"' + dateValue + '\\")"'
     def index  = sh(script: s, returnStdout: true);
     println "xxxx ${index}"
-    if(index == 'null') {
+    if(index == null) {
         println "${s} failed"
         currentBuild.result = 'FAILURE'
+    } else {
+        println "load ${dateValue} found"
     }
 }
