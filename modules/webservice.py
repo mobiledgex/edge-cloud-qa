c10bbf2918d7aa1622b275a55c860b26ac756548
Andy Anderson <andy.anderson@mobiledgex.com>
2022-04-05 14:21:26 -0500

    webservice changes for multi apps 	modified:   webservice.py

--- modules/webservice.py
+++ modules/webservice.py
@@ -6,21 +6,22 @@ import sys
 import os
 import logging.config
 import logging
-#import robot.api.logger
-#import requests.packages.urllib3.connectionpool as httplib
-#import httplib as http_client
+# import robot.api.logger
+# import requests.packages.urllib3.connectionpool as httplib
+# import httplib as http_client
 import http.client as http_client
 import time
-from robot.libraries.BuiltIn import BuiltIn
+# from robot.libraries.BuiltIn import BuiltIn
 
 logger = logging.getLogger(__name__)
 logging.getLogger('urllib3').setLevel(logging.CRITICAL)
 
-class WebService() :
+
+class WebService():
     resp = None
-    #protocol = 'https'
-    #output_format = 'json'
-    #httpLogger = None
+    # protocol = 'https'
+    # output_format = 'json'
+    # httpLogger = None
     debug = False
     trace_file = None
     stream_output_bytes = []
@@ -28,13 +29,13 @@ class WebService() :
     stream_output = []
     resp_text = ''
 
-    #def __init__(self, jid = None, password = None, sesid = None, http_or_https = None, output_format = None) :
-    def __init__(self, debug = False, http_trace = False) :
+    # def __init__(self, jid = None, password = None, sesid = None, http_or_https = None, output_format = None) :
+    def __init__(self, debug=False, http_trace=False):
         requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-        #logging.config.fileConfig('logging.ini')
+        # logging.config.fileConfig('logging.ini')
 
-        #logging.debug("init")
+        # logging.debug("init")
 
         self.debug = debug
         self.http_trace = http_trace
@@ -45,25 +46,25 @@ class WebService() :
         if debug:
             http_client.HTTPConnection.debuglevel = 1
 
-        #if http_or_https is not None:
+        # if http_or_https is not None:
         #    self.protocol = http_or_https
-        #if output_format is not None:
+        # if output_format is not None:
         #    self.output_format = output_format
 
     def post(self, url, data=None, verify_cert=False, headers=None, files=None, stream=False, connection_timeout=3.05, stream_timeout=60):
         logger.debug(f'post url={url} data={data} headers={headers} verify_cert={verify_cert} stream={stream} connection_timeout={connection_timeout}, stream_timeout={stream_timeout}')
-       
-        #url_to_use = self._buildUrl(url)
+
+        # url_to_use = self._buildUrl(url)
         self.stream_output_bytes = []
         self.stream_output = []
         self.stream_output_str = []
         self.resp_text = ''
         self.resp_dict = {}
- 
+
         timeout = (connection_timeout, stream_timeout)
-        #if stream and stream_timeout:
+        # if stream and stream_timeout:
         #    timeout = (3.05, int(stream_timeout))
-        
+
         try:
             self.resp = requests.post(url, data, verify=verify_cert, headers=headers, files=files, stream=stream, timeout=timeout)
             logger.debug(f'post resp={self.resp} stream={stream}')
@@ -71,10 +72,10 @@ class WebService() :
                 logger.debug('checking for stream output')
                 for line in self.resp.iter_lines():
                     logging.debug(f'stream line={line}')
-                    #BuiltIn().log_to_console(f'console stream line={line}')
+                    # BuiltIn().log_to_console(f'console stream line={line}')
                     self.stream_output_str.append(line.decode("utf-8"))
                     self.stream_output.append(json.loads(line.decode("utf-8")))
-                    #self.stream_output_bytes.append(line)
+                    # self.stream_output_bytes.append(line)
                 logger.debug('stream_output_bytes=' + str(self.stream_output_bytes))
                 self.resp_text = str(self.stream_output_str)
             else:
@@ -91,7 +92,7 @@ class WebService() :
                 raise Exception(f'POST ConnectionError exception: {e}')
         except Exception as e:
             raise Exception(f'POST exception: {e}')
-        
+
         if self.http_trace:
             self._print_trace()
 
@@ -99,9 +100,9 @@ class WebService() :
 
     def delete(self, url, verify_cert=False, headers=None):
         logging.debug('url=' + url)
-        #url_to_use = self._buildUrl(url)
+        # url_to_use = self._buildUrl(url)
         self.resp = requests.delete(url, verify=verify_cert, headers=headers)
-        #print requests.request
+        # print requests.request
         if self.http_trace:
             self._print_trace()
 
@@ -109,23 +110,23 @@ class WebService() :
 
     def get(self, url, data=None, verify_cert=False, headers=None, params=None):
         logging.debug('url=' + url + ' headers=' + str(headers) + ' data=' + str(data) + ' params=' + str(params))
-        #url_to_use = self._buildUrl(url, self.output_format)
-        #self.resp = requests.get(url_to_use, verify=verify_cert)
-        #print('headers=' + str(headers))
+        # url_to_use = self._buildUrl(url, self.output_format)
+        # self.resp = requests.get(url_to_use, verify=verify_cert)
+        # print('headers=' + str(headers))
         self.resp = requests.get(url, verify=verify_cert, headers=headers, data=data, params=params)
 
         if self.http_trace:
             self._print_trace()
 
-        #print(self.resp.request.url, self.resp.request.headers, self.resp.request.body)
+        # print(self.resp.request.url, self.resp.request.headers, self.resp.request.body)
 
         return self.resp
 
     def put(self, url, data=None, verify_cert=False, headers=None):
 
-        #url_to_use = self._buildUrl(url)
+        # url_to_use = self._buildUrl(url)
         self.resp = requests.put(url, data, verify=verify_cert, headers=headers)
-        #print requests.request
+        # print requests.request
 
         if self.http_trace:
             self._print_trace()
@@ -137,7 +138,7 @@ class WebService() :
 
     def get_output(self, key):
         return self.resp_dict[key]
-    
+
     def _build_parms(self, parms):
         s = ''
         for p in parms:
@@ -151,20 +152,20 @@ class WebService() :
         del arglist['self']
         arglist_temp = arglist.copy()
         for a in arglist:
-            if arglist[a] == None:
+            if arglist[a] is None:
                 del arglist_temp[a]
         parm_string = self._build_parms(arglist_temp)
         return parm_string
-            
+
     def _print_trace(self):
-        #file = os.path.basename(sys.argv[0])
+        # file = os.path.basename(sys.argv[0])
         if WebService.trace_file:
             WebService.trace_file = open(os.path.basename(sys.argv[-1]) + '.trace', 'a')
         else:
             WebService.trace_file = open(os.path.basename(sys.argv[-1]) + '.trace', 'w')
         WebService.trace_file.write('==================================================================\n\n')
         WebService.trace_file.write('REQUEST: ' + time.strftime("%d %b %Y %H:%M:%S", time.localtime()) + '\n')
-        #WebService.httpLogger.info('   GET ' + self.resp.request.url)
+        # WebService.httpLogger.info('   GET ' + self.resp.request.url)
         WebService.trace_file.write('   ' + self.resp.request.method + ' ' + self.resp.request.url + '\n')
         WebService.trace_file.write('      headers: ' + str(self.resp.request.headers) + '\n')
         WebService.trace_file.write('      body: ' + str(self.resp.request.body) + '\n')
@@ -172,7 +173,7 @@ class WebService() :
         WebService.trace_file.write('   status_code:' + str(self.resp.status_code) + '\n')
         WebService.trace_file.write('   body:' + self.resp.text + '\n')
         WebService.trace_file.close()
-        
+
 #    def _buildUrl(self, url, output_format = None):
 #        if url.startswith('http'):
 #            url_to_use = url
