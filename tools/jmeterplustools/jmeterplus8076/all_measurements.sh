f4051e98dfff941f7c9b93af39fdf55f325ecaa6
Jon Gainsley <jon.gainsley@mobiledgex.com>
2022-04-12 13:45:36 -0700

    Open-source under apache license

--- tools/jmeterplustools/jmeterplus8076/all_measurements.sh
+++ tools/jmeterplustools/jmeterplus8076/all_measurements.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2022 MobiledgeX, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 printf "\nSpecify one two three or four  args $1 $2 $3 $4 for all_measurements.sh <duration seconds> <cluster url of appinst> <port> <delay seconds for jmeter to start> example sh all_measurements.sh 50 testappalertcluster.automationdallascloudlet.packet.mobiledgex.net 10000  35 \n\n "
 #Clear out nohup.out before test begins set file size to zero
 command true > 'nohup.out'
