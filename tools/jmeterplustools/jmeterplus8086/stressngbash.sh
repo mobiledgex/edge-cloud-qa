c93bd479d7b2b4809641092d32193542c1dd990c
Jon Gainsley <jon.gainsley@mobiledgex.com>
2022-04-12 13:45:36 -0700

    Open-source under apache license

--- tools/jmeterplustools/jmeterplus8086/stressngbash.sh
+++ tools/jmeterplustools/jmeterplus8086/stressngbash.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2022 MobiledgeX, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 clear
        echo "./stressngbash.sh 1 1 1 1 1 128M 30 1  values are as follows:"
        echo "There are no checks to the values so be careful - not intended for human consumption"
