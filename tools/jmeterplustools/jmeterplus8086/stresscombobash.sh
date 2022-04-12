26223e0a19429f55a27edc6e7911f43b61630bed
Jon Gainsley <jon.gainsley@mobiledgex.com>
2022-04-12 13:45:36 -0700

    Open-source under apache license

--- tools/jmeterplustools/jmeterplus8086/stresscombobash.sh
+++ tools/jmeterplustools/jmeterplus8086/stresscombobash.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2022 MobiledgeX, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #$1 number of times to run, $2 sleep before test start in seconds, $3 durration of the trigger time and wait to clear, use $2 to tweak resting time if you want a longer wait duration
 #$1=loop $2=sleep $3=timeout and sleep --cpu $4 --hdd $5 --vm $6 --io $7 defult stress-ng --cpu 4 --hdd 4 --vm 4 --io 4 --metrics-brief --timeout 35
 #while $1; use true and remove for to make run all the time with same timings
--- tools/run_automation_privacyPolicy_server.sh
+++ tools/run_automation_privacyPolicy_server.sh
@@ -1,2 +1,16 @@
+# Copyright 2022 MobiledgeX, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # used on mexdemo.locsim.mobiledgex.net for egress port testing
 docker run --name egress_simulator -d -p 2016:2016/udp -p 2015:2015/udp -p 2016:2016 -p 2015:2015 docker-qa.mobiledgex.net/mobiledgex/images/server_ping_threaded:11.0
